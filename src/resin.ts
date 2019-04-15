12bab4ff126f8156afa8aa1ad62f8776746d7c6e
Pagan Gazzard <page@resin.io>
2019-04-15 18:15:57 +0100

    Handle `my_applications` within pinejs
    Change-type: minor

---
+++ src/resin.ts
@@ -0,0 +1,10 @@
+import * as _ from 'lodash';
+import { generateAbstractSqlModel, aliasTable } from './abstract-sql-utils';
+
+export const apiRoot = 'resin';
+export const modelName = 'resin';
+export const migrationsPath = __dirname + '/migrations/';
+export const initSqlPath = __dirname + '/resin-init.sql';
+export const abstractSql = generateAbstractSqlModel(__dirname + '/resin.sbvr');
+
+aliasTable(abstractSql, 'application', 'my application');
