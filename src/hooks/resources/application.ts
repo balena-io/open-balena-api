d66a8bb9b76c447884b4c73c7e0327ef38fbc97c
Pagan Gazzard <page@balena.io>
2019-10-18 22:16:35 +0100

    Simplify application hook

--- src/hooks/resources/application.ts
+++ src/hooks/resources/application.ts
@@ -112,29 +112,24 @@ sbvrUtils.addPureHook('PATCH', 'resin', 'application', {
 
 		return Promise.all(waitPromises);
 	},
-	POSTRUN: args => {
-		const { request } = args;
-		const waitPromises = [];
+	POSTRUN: ({ request }) => {
 		if (request.values.should_be_running__release != null) {
 			// Only update apps if they have had their release changed.
-			waitPromises.push(
-				request.custom.affectedIds.then((ids: number[]) => {
-					if (ids.length === 0) {
-						return;
-					}
-					postDevices({
-						url: '/v1/update',
-						req: root,
-						filter: {
-							belongs_to__application: { $in: ids },
-						},
-						// Don't wait for the posts to complete, as they may take a long time and we've already sent the prompt to update.
-						wait: false,
-					});
-				}),
-			);
+			return request.custom.affectedIds.then((ids: number[]) => {
+				if (ids.length === 0) {
+					return;
+				}
+				postDevices({
+					url: '/v1/update',
+					req: root,
+					filter: {
+						belongs_to__application: { $in: ids },
+					},
+					// Don't wait for the posts to complete, as they may take a long time and we've already sent the prompt to update.
+					wait: false,
+				});
+			});
 		}
-		return Promise.all(waitPromises);
 	},
 });
 
