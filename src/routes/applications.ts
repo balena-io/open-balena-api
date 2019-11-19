bed2256f222ba3c37607ba2e8ec135404c8b93cb
Pagan Gazzard <page@balena.io>
2019-11-19 21:13:20 +0000

    Convert routes/applications to async/await
    Change-type: patch

--- src/routes/applications.ts
+++ src/routes/applications.ts
@@ -1,7 +1,5 @@
 import * as _ from 'lodash';
 
-import * as Promise from 'bluebird';
-
 import { generateConfig } from '../lib/device-config';
 import { findBySlug } from '../lib/device-types';
 
@@ -15,32 +13,31 @@ import { RequestHandler, Request } from 'express';
 
 const { UnauthorizedError, api } = sbvrUtils;
 
-const getApp = (req: Request): Promise<AnyObject> =>
-	api.resin
-		.get({
-			resource: 'application',
-			id: req.param('appId'),
-			passthrough: { req },
-			options: {
-				$select: ['id', 'app_name'],
-				$expand: {
-					is_for__device_type: {
-						$select: ['slug'],
-					},
+const getApp = async (req: Request): Promise<AnyObject> => {
+	const app = (await api.resin.get({
+		resource: 'application',
+		id: req.param('appId'),
+		passthrough: { req },
+		options: {
+			$select: ['id', 'app_name'],
+			$expand: {
+				is_for__device_type: {
+					$select: ['slug'],
 				},
 			},
-		})
-		.then((app: AnyObject) => {
-			// Check that the current user has access to this application.
-			if (app == null) {
-				throw new UnauthorizedError(
-					'You do not have permission to access this application',
-				);
-			}
-			return app;
-		});
+		},
+	})) as AnyObject;
 
-export const downloadImageConfig: RequestHandler = (req, res) => {
+	// Check that the current user has access to this application.
+	if (app == null) {
+		throw new UnauthorizedError(
+			'You do not have permission to access this application',
+		);
+	}
+	return app;
+};
+
+export const downloadImageConfig: RequestHandler = async (req, res) => {
 	if (!req.param('appId')) {
 		res.status(400).send('An appId is required.');
 		return;
@@ -54,26 +51,26 @@ export const downloadImageConfig: RequestHandler = (req, res) => {
 		return;
 	}
 
-	const resinApi = api.resin.clone({ passthrough: { req } });
+	try {
+		const resinApi = api.resin.clone({ passthrough: { req } });
 
-	return getApp(req)
-		.then(app =>
-			findBySlug(
-				resinApi,
-				deviceTypeSlug || app.is_for__device_type[0].slug,
-			).then(deviceType => generateConfig(req, app, deviceType, osVersion)),
-		)
-		.then(config => {
-			res.json(config);
-		})
-		.catch(UnauthorizedError, err => {
+		const app = await getApp(req);
+		const deviceType = await findBySlug(
+			resinApi,
+			deviceTypeSlug || app.is_for__device_type[0].slug,
+		);
+		const config = await generateConfig(req, app, deviceType, osVersion);
+
+		res.json(config);
+	} catch (err) {
+		if (err instanceof UnauthorizedError) {
 			res.status(404).send(err.message);
-		})
-		.catch(err => {
-			if (handleHttpErrors(req, res, err)) {
-				return;
-			}
-			captureException(err, 'Error generating config', { req });
-			res.status(500).send(translateError(err));
-		});
+			return;
+		}
+		if (handleHttpErrors(req, res, err)) {
+			return;
+		}
+		captureException(err, 'Error generating config', { req });
+		res.status(500).send(translateError(err));
+	}
 };
