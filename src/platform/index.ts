195d242035d56bebf3e02dc2f2cb40e85c436dc1
Pagan Gazzard <page@balena.io>
2019-12-27 14:49:23 +0000

    Convert platform/index to async/await
    Change-type: patch

--- src/platform/index.ts
+++ src/platform/index.ts
@@ -20,109 +20,100 @@ if (sbvrUtils.db.readTransaction == null) {
 
 // TODO: Potential races here. They are unlikely but not impossible. Will fix
 // in subsequent PR.
-const $getOrInsertId = (
+const $getOrInsertId = async (
 	api: sbvrUtils.PinejsClient,
 	resource: string,
 	body: AnyObject,
 	tx?: Tx,
-): Bluebird<{ id: number }> => {
+): Promise<{ id: number }> => {
 	const apiTx = api.clone({ passthrough: { req: root, tx } });
-	return apiTx
-		.get({
+	const results = (await apiTx.get({
+		resource,
+		options: {
+			$select: 'id',
+			$filter: body,
+		},
+	})) as Array<{ id: number }>;
+	if (results.length === 0) {
+		const idObj = (await apiTx.post({
 			resource,
-			options: {
-				$select: 'id',
-				$filter: body,
-			},
-		})
-		.then((results: AnyObject[]) => {
-			if (results.length === 0) {
-				return apiTx
-					.post({
-						resource,
-						body,
-						options: { returnResource: false },
-					})
-					.then((idObj: { id: number }) => {
-						return { ...idObj, ...body };
-					});
-			} else {
-				return results[0] as { id: number };
-			}
-		});
+			body,
+			options: { returnResource: false },
+		})) as { id: number };
+		return { ...idObj, ...body };
+	} else {
+		return results[0];
+	}
 };
 
 // Given a filter, if a resource exists which supports said filter,
 // update it to the values specified in updateFields, otherwise
 // insert it with a combination of the filter and updateFields value
-const $updateOrInsert = (
+const $updateOrInsert = async (
 	api: PinejsClient,
 	resource: string,
 	filter: PinejsClientCoreFactory.FilterObj,
 	updateFields: AnyObject,
 	tx?: Tx,
-): Bluebird<{ id: number }> => {
+): Promise<{ id: number }> => {
 	const apiTx = api.clone({ passthrough: { req: root, tx } });
-	return apiTx
-		.get({
+	const results = (await apiTx.get({
+		resource,
+		options: {
+			$filter: filter,
+			$select: ['id'],
+		},
+	})) as Array<{ id: number }>;
+
+	if (results.length === 0) {
+		const body = _.cloneDeep(filter);
+		_.merge(body, updateFields);
+		return apiTx.post({
 			resource,
-			options: {
-				$filter: filter,
-				$select: ['id'],
-			},
-		})
-		.then((results: AnyObject[]) => {
-			if (results.length === 0) {
-				const body = _.cloneDeep(filter);
-				_.merge(body, updateFields);
-				return apiTx.post({
-					resource,
-					body,
-					options: { returnResource: false },
-				}) as Bluebird<{ id: number }>;
-			} else if (results.length > 1) {
-				throw new Error(
-					`updateOrInsert filter not unique for '${resource}': '${JSON.stringify(
-						filter,
-					)}'`,
-				);
-			} else {
-				// do a patch with the id
-				return apiTx
-					.patch({
-						resource,
-						id: results[0].id,
-						body: updateFields,
-					})
-					.return(results[0] as { id: number });
-			}
+			body,
+			options: { returnResource: false },
+		}) as Promise<{ id: number }>;
+	} else if (results.length > 1) {
+		throw new Error(
+			`updateOrInsert filter not unique for '${resource}': '${JSON.stringify(
+				filter,
+			)}'`,
+		);
+	} else {
+		// do a patch with the id
+		await apiTx.patch({
+			resource,
+			id: results[0].id,
+			body: updateFields,
 		});
+		return results[0];
+	}
 };
 
 export const getOrInsertId = (
 	resource: string,
 	body: AnyObject,
 	tx?: Tx,
-): Bluebird<{ id: number }> => $getOrInsertId(api.Auth, resource, body, tx);
+): Promise<{ id: number }> => $getOrInsertId(api.Auth, resource, body, tx);
 export const getOrInsertModelId = (
 	resource: string,
 	body: AnyObject,
 	tx?: Tx,
-): Bluebird<{ id: number }> => $getOrInsertId(api.resin, resource, body, tx);
+): Promise<{ id: number }> => $getOrInsertId(api.resin, resource, body, tx);
 
 export const updateOrInsert = (
 	resource: string,
 	filter: PinejsClientCoreFactory.FilterObj,
 	updateFields: AnyObject,
 	tx?: Tx,
-): Bluebird<{ id: number }> =>
+): Promise<{ id: number }> =>
 	$updateOrInsert(api.Auth, resource, filter, updateFields, tx);
 export const updateOrInsertModel = (
 	resource: string,
 	filter: PinejsClientCoreFactory.FilterObj,
 	updateFields: AnyObject,
 	tx?: Tx,
-): Bluebird<{ id: number }> =>
+): Promise<{ id: number }> =>
 	$updateOrInsert(api.resin, resource, filter, updateFields, tx);
 
 type TxFn = (tx: Tx, ...args: any[]) => PromiseLike<any>;
@@ -164,20 +155,19 @@ export const getCurrentRequestAffectedIds: typeof sbvrUtils.getAffectedIds = arg
 	return args.request.custom.affectedIds;
 };
 
-export const createActor = ({
+export const createActor = async ({
 	request,
 	tx,
-}: sbvrUtils.HookArgs): Bluebird<void> => {
-	return api.Auth.post({
+}: sbvrUtils.HookArgs): Promise<void> => {
+	const result = (await api.Auth.post({
 		resource: 'actor',
 		passthrough: {
 			tx,
 			req: root,
 		},
 		options: { returnResource: false },
-	}).then((result: AnyObject) => {
-		request.values.actor = result.id;
-	});
+	})) as AnyObject;
+	request.values.actor = result.id;
 };
 
 export function addDeleteHookForDependents(
@@ -185,38 +175,35 @@ export function addDeleteHookForDependents(
 	dependents: Array<[string, string]>,
 ) {
 	sbvrUtils.addPureHook('DELETE', 'resin', resource, {
-		PRERUN: args => {
+		PRERUN: async args => {
 			const { api, req } = args;
 
-			return getCurrentRequestAffectedIds(args).then(resourceIds => {
-				if (resourceIds.length === 0) {
-					return;
-				}
+			const resourceIds = await getCurrentRequestAffectedIds(args);
+			if (resourceIds.length === 0) {
+				return;
+			}
 
-				return Bluebird.mapSeries(
-					dependents,
-					([dependent, resourceIdField]) => {
-						return api
-							.delete({
-								resource: dependent,
-								options: {
-									$filter: {
-										[resourceIdField]: { $in: resourceIds },
-									},
-								},
-							})
-							.tapCatch(err => {
-								captureException(
-									err,
-									`Error deleting resource '${dependent}' before deleting '${resource}' `,
-									{
-										req,
-									},
-								);
-							});
-					},
-				).return();
-			});
+			for (const [dependent, resourceIdField] of dependents) {
+				try {
+					await api.delete({
+						resource: dependent,
+						options: {
+							$filter: {
+								[resourceIdField]: { $in: resourceIds },
+							},
+						},
+					});
+				} catch (err) {
+					captureException(
+						err,
+						`Error deleting resource '${dependent}' before deleting '${resource}' `,
+						{
+							req,
+						},
+					);
+					throw err;
+				}
+			}
 		},
 	});
 }
