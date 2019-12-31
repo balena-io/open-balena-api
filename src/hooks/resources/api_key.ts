953181bc49e836a2bf5f676714f48e738f350304
Pagan Gazzard <page@balena.io>
2019-12-31 14:58:34 +0000

    Convert hooks/resources/api_key to async/await
    Change-type: patch

--- src/hooks/resources/api_key.ts
+++ src/hooks/resources/api_key.ts
@@ -2,36 +2,36 @@ import { sbvrUtils } from '@resin/pinejs';
 import { getCurrentRequestAffectedIds } from '../../platform';
 
 import { captureException } from '../../platform/errors';
-import * as Promise from 'bluebird';
+import * as Bluebird from 'bluebird';
 
 const { root, api } = sbvrUtils;
 
 const deleteApiKeyHooks: sbvrUtils.Hooks = {
-	PRERUN: args =>
-		getCurrentRequestAffectedIds(args).then(keyIds => {
-			if (keyIds.length === 0) {
-				return;
-			}
+	PRERUN: async args => {
+		const keyIds = await getCurrentRequestAffectedIds(args);
+		if (keyIds.length === 0) {
+			return;
+		}
 
-			return Promise.map(
-				['api_key__has__role', 'api_key__has__permission'],
-				resource =>
-					api.Auth.delete({
-						resource,
-						passthrough: {
-							tx: args.tx,
-							req: root,
-						},
-						options: {
-							$filter: { api_key: { $in: keyIds } },
-						},
-					}).tapCatch(err => {
-						captureException(err, 'Error deleting api key ' + resource, {
-							req: args.req,
-						});
-					}),
-			).return();
-		}),
+		await Bluebird.map(
+			['api_key__has__role', 'api_key__has__permission'],
+			resource =>
+				api.Auth.delete({
+					resource,
+					passthrough: {
+						tx: args.tx,
+						req: root,
+					},
+					options: {
+						$filter: { api_key: { $in: keyIds } },
+					},
+				}).tapCatch(err => {
+					captureException(err, 'Error deleting api key ' + resource, {
+						req: args.req,
+					});
+				}),
+		);
+	},
 };
 
 sbvrUtils.addPureHook('DELETE', 'Auth', 'api_key', deleteApiKeyHooks);
