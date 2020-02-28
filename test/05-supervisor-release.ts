5351b72bd0d2e8c41f8f41a274ee38824ed2d198
Cameron Diver <cameron@balena.io>
2020-02-28 13:14:51 +0700

    Don't allow downgrading a supervisor release for a device
    Change-type: patch
    Signed-off-by: Cameron Diver <cameron@balena.io>

--- test/05-supervisor-release.ts
+++ test/05-supervisor-release.ts
@@ -4,6 +4,8 @@ import { supertest, UserObjectParam } from './test-lib/supertest';
 
 import { app } from '../init';
 
+import * as fakeDevice from './test-lib/fake-device';
+
 describe('supervisor release', function() {
 	let admin: UserObjectParam;
 
@@ -50,5 +52,105 @@ describe('supervisor release', function() {
 		await supertest(app, admin)
 			.delete(`/resin/supervisor_release(${supervisorReleaseId})`)
 			.expect(200);
+	});
+});
+
+describe('Devices running supervisor releases', () => {
+	let admin: UserObjectParam;
+	let applicationId: number;
+	let device: fakeDevice.Device;
+	let small: number;
+	let middle: number;
+	let large: number;
+	before(async () => {
+		const fx = await fixtures.load('05-supervisor-release');
+		admin = fx.users.admin;
+		applicationId = fx.applications.app1.id;
+
+		device = await fakeDevice.provisionDevice(admin, applicationId);
+
+		// let's create some releases
+		const res = await supertest(app)
+			.get(`/resin/device_type?$select=id&$filter=slug eq 'intel-nuc'`)
+			.expect(200);
+
+		expect(res.body)
+			.to.have.nested.property('d[0].id')
+			.that.is.a('number');
+
+		middle = (
+			await supertest(app, admin)
+				.post(`/resin/supervisor_release`)
+				.send({
+					image_name: 'SOME_IMAGE',
+					supervisor_version: '7.0.1',
+					is_for__device_type: res.body.d[0].id,
+				})
+				.expect(201)
+		).body.id;
+
+		large = (
+			await supertest(app, admin)
+				.post(`/resin/supervisor_release`)
+				.send({
+					image_name: 'SOME_IMAGE',
+					supervisor_version: '8.0.1',
+					is_for__device_type: res.body.d[0].id,
+				})
+				.expect(201)
+		).body.id;
+
+		small = (
+			await supertest(app, admin)
+				.post(`/resin/supervisor_release`)
+				.send({
+					image_name: 'SOME_IMAGE',
+					supervisor_version: '6.0.1_logstream',
+					is_for__device_type: res.body.d[0].id,
+				})
+				.expect(201)
+		).body.id;
+	});
+
+	it('should allow setting a device to a supervisor release', async () => {
+		await supertest(app, admin)
+			.patch(`/resin/device(${device.id})`)
+			.send({
+				should_be_managed_by__supervisor_release: small,
+			})
+			.expect(200);
+	});
+
+	it('should allow updrading a devices supervisor release', async () => {
+		await supertest(app, admin)
+			.patch(`/resin/device(${device.id})`)
+			.send({
+				should_be_managed_by__supervisor_release: middle,
+			})
+			.expect(200);
+		await supertest(app, admin)
+			.patch(`/resin/device(${device.id})`)
+			.send({
+				should_be_managed_by__supervisor_release: large,
+			})
+			.expect(200);
+	});
+
+	it('should not allow downgrading a supervisor version', async () => {
+		await supertest(app, admin)
+			.patch(`/resin/device(${device.id})`)
+			.send({
+				should_be_managed_by__supervisor_release: middle,
+			})
+			.expect(400);
+	});
+
+	it('should correctly determine logstream values', async () => {
+		await supertest(app, admin)
+			.patch(`/resin/device(${device.id})`)
+			.send({
+				should_be_managed_by__supervisor_release: small,
+			})
+			.expect(400);
 	});
 });
