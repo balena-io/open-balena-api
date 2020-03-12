516a8af03db42071b686d904c74d67a57850c1a1
Pagan Gazzard <page@balena.io>
2020-03-12 10:57:39 +0000

    Tests: make device types available via fixtures
    Change-type: patch

---
+++ test/test-lib/device-type.ts
@@ -0,0 +1,38 @@
+import { sbvrUtils } from '@resin/pinejs';
+import { setDefaultFixtures } from './fixtures';
+
+const { api, root } = sbvrUtils;
+
+interface DeviceType {
+	id: number;
+	slug: string;
+	name: string;
+}
+
+setDefaultFixtures(
+	'deviceTypes',
+	(new Proxy({} as Dictionary<DeviceType>, {
+		get: async (obj, slug) => {
+			if (typeof slug === 'string' && !obj.hasOwnProperty(slug)) {
+				if (slug === 'then') {
+					// Something is checking if we're a thenable
+					return;
+				}
+				const deviceTypes = (await api.resin.get({
+					resource: 'device_type',
+					passthrough: {
+						req: root,
+					},
+					options: {
+						$select: ['id', 'slug', 'name'],
+						$filter: {
+							slug,
+						},
+					},
+				})) as DeviceType[];
+				obj[slug] = deviceTypes[0];
+			}
+			return obj[slug as any];
+		},
+	}) as any) as Dictionary<Promise<DeviceType>>,
+);
