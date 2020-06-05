54b7cbdf310b8af2296661c37aeab20dd71b9565
Thodoris Greasidis <thgreasi@gmail.com>
2020-06-05 18:34:58 +0300

    test-lib/fake-device: Extract the fake Device type from the return value
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- test/test-lib/fake-device.ts
+++ test/test-lib/fake-device.ts
@@ -7,14 +7,6 @@ import { app } from '../../init';
 
 import { supertest, UserObjectParam } from './supertest';
 
-export interface Device {
-	id: number;
-	uuid: string;
-	token: string;
-	getStateV2: () => Promise<DeviceState>;
-	patchStateV2: (devicePatchBody: AnyObject) => Promise<void>;
-}
-
 interface DeviceStateApp {
 	name: string;
 	commit: string;
@@ -44,10 +36,7 @@ export interface DeviceState {
 	};
 }
 
-export async function provisionDevice(
-	admin: UserObjectParam,
-	appId: number,
-): Promise<Device> {
+export async function provisionDevice(admin: UserObjectParam, appId: number) {
 	const { body: applications } = await supertest(app, admin)
 		.get(`/resin/application(${appId})?$expand=is_for__device_type`)
 		.expect(200);
@@ -65,7 +54,7 @@ export async function provisionDevice(
 
 	const deviceType: string = applications.d[0].is_for__device_type[0].slug;
 
-	const { body: device } = await supertest(app, admin)
+	const { body: deviceEntry } = await supertest(app, admin)
 		.post('/resin/device')
 		.send({
 			belongs_to__application: appId,
@@ -76,7 +65,30 @@ export async function provisionDevice(
 		})
 		.expect(201);
 
-	device.token = randomstring.generate(16);
+	const device = {
+		...(deviceEntry as {
+			id: number;
+			uuid: string;
+		}),
+		token: randomstring.generate(16),
+		getStateV2: async (): Promise<DeviceState> => {
+			const { body: state } = await supertest(app, device)
+				.get(`/device/v2/${device.uuid}/state`)
+				.expect(200);
+
+			expect(state).to.have.property('local');
+			expect(state.local).to.have.property('name');
+			expect(state.local).to.have.property('config');
+
+			return state;
+		},
+		patchStateV2: async (devicePatchBody: AnyObject) => {
+			await supertest(app, device)
+				.patch(`/device/v2/${device.uuid}/state`)
+				.send(devicePatchBody)
+				.expect(200);
+		},
+	};
 
 	await supertest(app, admin)
 		.post(`/api-key/device/${device.id}/device-key`)
@@ -85,24 +97,7 @@ export async function provisionDevice(
 		})
 		.expect(200);
 
-	device.getStateV2 = async (): Promise<DeviceState> => {
-		const { body: state } = await supertest(app, device)
-			.get(`/device/v2/${device.uuid}/state`)
-			.expect(200);
-
-		expect(state).to.have.property('local');
-		expect(state.local).to.have.property('name');
-		expect(state.local).to.have.property('config');
-
-		return state;
-	};
-
-	device.patchStateV2 = async (devicePatchBody: AnyObject): Promise<void> => {
-		await supertest(app, device)
-			.patch(`/device/v2/${device.uuid}/state`)
-			.send(devicePatchBody)
-			.expect(200);
-	};
-
 	return device;
 }
+
+export type Device = ResolvableReturnType<typeof provisionDevice>;
