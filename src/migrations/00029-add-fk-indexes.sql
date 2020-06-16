6b3c0456031e45bc141a187ddb4a4867b57c24f5
Pagan Gazzard <page@balena.io>
2020-06-16 15:16:32 +0100

    Update migration to create all indexes normally
    This allows them to be run inside a transaction
    
    Change-type: patch

--- src/migrations/00029-add-fk-indexes.sql
+++ src/migrations/00029-add-fk-indexes.sql
@@ -1,14 +1,14 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS "application_release_idx"
+CREATE INDEX IF NOT EXISTS "application_release_idx"
 ON "application" ("should be running-release");
-CREATE INDEX CONCURRENTLY IF NOT EXISTS "application_application_type_idx"
+CREATE INDEX IF NOT EXISTS "application_application_type_idx"
 ON "application" ("application type");
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS "device_is_running_release_idx"
+CREATE INDEX IF NOT EXISTS "device_is_running_release_idx"
 ON "device" ("is running-release");
-CREATE INDEX CONCURRENTLY IF NOT EXISTS "device_should_be_running_release_idx"
+CREATE INDEX IF NOT EXISTS "device_should_be_running_release_idx"
 ON "device" ("should be running-release");
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS "gateway_download_device_idx"
+CREATE INDEX IF NOT EXISTS "gateway_download_device_idx"
 ON "gateway download" ("is downloaded by-device");
 
 CREATE INDEX IF NOT EXISTS "service_install_service_idx"
