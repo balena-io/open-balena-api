7cdf86daab2c82302b1e63e276b4b8f05a8ea1f7
Pagan Gazzard <page@balena.io>
2020-08-27 12:57:48 +0100

    Move device move hooks into the devices feature
    Change-type: patch

--- src/hooks/resources/device.ts
+++ src/hooks/resources/device.ts
@@ -1,70 +1,15 @@
 import * as _ from 'lodash';
-import { TypedError } from 'typed-error';
 
 import { sbvrUtils, hooks, permissions, errors } from '@balena/pinejs';
 
-import {
-	checkDevicesCanBeInApplication,
-	checkDevicesCanHaveDeviceURL,
-} from '../../features/application-types/application-types';
+import { checkDevicesCanHaveDeviceURL } from '../../features/application-types/application-types';
 
 const { BadRequestError } = errors;
-
-export class InaccessibleAppError extends TypedError {
-	constructor(
-		message = "Application doesn't exist or you have no access to it.",
-	) {
-		super(message);
-	}
-}
 
 hooks.addPureHook('PATCH', 'resin', 'device', {
 	PRERUN: async (args) => {
 		const { api, request } = args;
 		const waitPromises: Array<PromiseLike<any>> = [];
-
-		if (request.values.belongs_to__application != null) {
-			waitPromises.push(
-				api
-					.get({
-						resource: 'application',
-						id: request.values.belongs_to__application,
-						options: {
-							$select: 'id',
-						},
-					})
-					.catch(() => {
-						throw new InaccessibleAppError();
-					})
-					.then(async (app) => {
-						if (app == null) {
-							throw new InaccessibleAppError();
-						}
-
-						const deviceIds = await sbvrUtils.getAffectedIds(args);
-						if (deviceIds.length === 0) {
-							return;
-						}
-						// and get the devices being affected and store them for the POSTRUN...
-						const devices = (await args.api.get({
-							resource: 'device',
-							options: {
-								$select: 'id',
-								$filter: {
-									id: {
-										$in: deviceIds,
-									},
-									belongs_to__application: {
-										$ne: args.request.values.belongs_to__application,
-									},
-								},
-							},
-						})) as Array<{ id: number }>;
-
-						request.custom.movedDevices = devices.map((device) => device.id);
-					}),
-			);
-		}
 
 		// check the release is valid for the devices affected...
 		if (request.values.should_be_running__release != null) {
@@ -125,24 +70,9 @@ hooks.addPureHook('PATCH', 'resin', 'device', {
 			);
 		}
 
-		if (request.values.belongs_to__application != null) {
-			waitPromises.push(
-				sbvrUtils
-					.getAffectedIds(args)
-					.then((deviceIds) =>
-						checkDevicesCanBeInApplication(
-							api,
-							request.values.belongs_to__application,
-							deviceIds,
-						),
-					),
-			);
-		}
-
 		await Promise.all(waitPromises);
 	},
 	POSTRUN: async (args) => {
-		const waitPromises: Array<PromiseLike<any>> = [];
 		const affectedIds = args.request.affectedIds!;
 
 		// We only want to set dependent devices offline when the gateway goes
@@ -150,45 +80,18 @@ hooks.addPureHook('PATCH', 'resin', 'device', {
 		// device back to online as need be.
 		const isOnline = args.request.values.is_online;
 		if ([false, 0].includes(isOnline) && affectedIds.length !== 0) {
-			waitPromises.push(
-				args.api.patch({
-					resource: 'device',
-					options: {
-						$filter: {
-							is_managed_by__device: { $in: affectedIds },
-							is_online: { $ne: isOnline },
-						},
+			await args.api.patch({
+				resource: 'device',
+				options: {
+					$filter: {
+						is_managed_by__device: { $in: affectedIds },
+						is_online: { $ne: isOnline },
 					},
-					body: {
-						is_online: isOnline,
-					},
-				}),
-			);
+				},
+				body: {
+					is_online: isOnline,
+				},
+			});
 		}
-
-		// We need to delete all service_install resources for the current device and
-		// create new ones for the new application (if the device is moving application)
-		if (args.request.values.belongs_to__application != null) {
-			// Also mark all image installs of moved devices as deleted because
-			// they're for the previous application.
-			const { movedDevices } = args.request.custom;
-			if (movedDevices.length > 0) {
-				waitPromises.push(
-					args.api.patch({
-						resource: 'image_install',
-						body: {
-							status: 'deleted',
-						},
-						options: {
-							$filter: {
-								device: { $in: movedDevices },
-							},
-						},
-					}),
-				);
-			}
-		}
-
-		await Promise.all(waitPromises);
 	},
 });
