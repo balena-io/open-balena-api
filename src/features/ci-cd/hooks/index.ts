3771e3fa4baba9bcea5516cdadfca80905d1d491
Pagan Gazzard <page@balena.io>
2020-08-27 12:41:05 +0100

    Move service install hooks into the ci-cd feature
    Change-type: patch

--- src/features/ci-cd/hooks/index.ts
+++ src/features/ci-cd/hooks/index.ts
@@ -2,4 +2,5 @@ import './app-update-trigger';
 import './device-update-trigger';
 import './image-is-stored-at-location';
 import './release-update-timestamp';
+import './service-installs';
 import './track-latest-release';
--- src/hooks/resources/device.ts
+++ src/hooks/resources/device.ts
@@ -2,7 +2,6 @@ import * as _ from 'lodash';
 import { TypedError } from 'typed-error';
 
 import { sbvrUtils, hooks, permissions, errors } from '@balena/pinejs';
-import type { Filter } from 'pinejs-client-core';
 
 import {
 	checkDevicesCanBeInApplication,
@@ -27,87 +26,6 @@ export const isDeviceNameValid = (name: string) => {
 	return !INVALID_NEWLINE_REGEX.test(name);
 };
 
-const createReleaseServiceInstalls = async (
-	api: sbvrUtils.PinejsClient,
-	deviceIds: number[],
-	releaseFilter: Filter,
-): Promise<void> => {
-	await Promise.all(
-		deviceIds.map(async (deviceId) => {
-			const services = await api.get({
-				resource: 'service',
-				options: {
-					$select: 'id',
-					$filter: {
-						is_built_by__image: {
-							$any: {
-								$alias: 'i',
-								$expr: {
-									i: {
-										is_part_of__release: {
-											$any: {
-												$alias: 'ipr',
-												$expr: {
-													ipr: {
-														release: {
-															$any: {
-																$alias: 'r',
-																$expr: { r: releaseFilter },
-															},
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-						// Filter out any services which do have a service install attached
-						$not: {
-							service_install: {
-								$any: {
-									$alias: 'si',
-									$expr: {
-										si: { device: deviceId },
-									},
-								},
-							},
-						},
-					},
-				},
-			});
-			await Promise.all(
-				services.map(async (service) => {
-					// Create a service_install for this pair of service and device
-					await api.post({
-						resource: 'service_install',
-						body: {
-							device: deviceId,
-							installs__service: service.id,
-						},
-						options: { returnResource: false },
-					});
-				}),
-			);
-		}),
-	);
-};
-
-const createAppServiceInstalls = async (
-	api: sbvrUtils.PinejsClient,
-	appId: number,
-	deviceIds: number[],
-): Promise<void> =>
-	createReleaseServiceInstalls(api, deviceIds, {
-		should_be_running_on__application: {
-			$any: {
-				$alias: 'a',
-				$expr: { a: { id: appId } },
-			},
-		},
-	});
-
 hooks.addPureHook('POST', 'resin', 'device', {
 	POSTPARSE: async ({ request }) => {
 		// Check for extra whitespace characters
@@ -119,8 +37,6 @@ hooks.addPureHook('POST', 'resin', 'device', {
 				'Device name cannot contain any newline characters.',
 			);
 		}
-		// Keep the app ID for later -- we'll need it in the POSTRUN hook
-		request.custom.appId = request.values.belongs_to__application;
 
 		request.values.device_name =
 			request.values.device_name || haikuName.generate();
@@ -132,16 +48,6 @@ hooks.addPureHook('POST', 'resin', 'device', {
 				'Device UUID must be a 32 or 62 character long lower case hex string.',
 			);
 		}
-	},
-	POSTRUN: async ({ request, api, tx, result: deviceId }) => {
-		// Don't try to add service installs if the device wasn't created
-		if (deviceId == null) {
-			return;
-		}
-
-		const rootApi = api.clone({ passthrough: { tx, req: permissions.root } });
-
-		await createAppServiceInstalls(rootApi, request.custom.appId, [deviceId]);
 	},
 });
 
@@ -339,27 +245,6 @@ hooks.addPureHook('PATCH', 'resin', 'device', {
 		// We need to delete all service_install resources for the current device and
 		// create new ones for the new application (if the device is moving application)
 		if (args.request.values.belongs_to__application != null) {
-			if (affectedIds.length !== 0) {
-				waitPromises.push(
-					args.api
-						.delete({
-							resource: 'service_install',
-							options: {
-								$filter: {
-									device: { $in: affectedIds },
-								},
-							},
-						})
-						.then(() =>
-							createAppServiceInstalls(
-								args.api,
-								args.request.values.belongs_to__application,
-								affectedIds,
-							),
-						),
-				);
-			}
-
 			// Also mark all image installs of moved devices as deleted because
 			// they're for the previous application.
 			const { movedDevices } = args.request.custom;
@@ -376,51 +261,6 @@ hooks.addPureHook('PATCH', 'resin', 'device', {
 							},
 						},
 					}),
-				);
-			}
-		}
-
-		if (
-			args.request.values.should_be_running__release !== undefined &&
-			affectedIds.length !== 0
-		) {
-			// If the device was preloaded, and then pinned, service_installs do not exist
-			// for this device+release combination. We need to create these
-			if (args.request.values.should_be_running__release != null) {
-				waitPromises.push(
-					createReleaseServiceInstalls(args.api, affectedIds, {
-						id: args.request.values.should_be_running__release,
-					}),
-				);
-			} else {
-				waitPromises.push(
-					(async () => {
-						const devices = (await args.api.get({
-							resource: 'device',
-							options: {
-								$select: ['id', 'belongs_to__application'],
-								$filter: {
-									id: { $in: affectedIds },
-								},
-							},
-						})) as Array<{
-							id: number;
-							belongs_to__application: { __id: number };
-						}>;
-						const devicesByApp = _.groupBy(
-							devices,
-							(d) => d.belongs_to__application.__id,
-						);
-						await Promise.all(
-							Object.keys(devicesByApp).map((appId) =>
-								createAppServiceInstalls(
-									args.api,
-									devicesByApp[appId][0].belongs_to__application.__id,
-									devicesByApp[appId].map((d) => d.id),
-								),
-							),
-						);
-					})(),
 				);
 			}
 		}
