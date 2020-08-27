6e26becb96c5602b4fa84738833db4f43b2b05d8
Pagan Gazzard <page@balena.io>
2020-08-27 12:49:26 +0100

    Move device validation/defaults hooks into a devices features

---
+++ src/features/devices/hooks/defaults-validation.ts
@@ -0,0 +1,79 @@
+import { hooks, errors } from '@balena/pinejs';
+import * as haikuName from '../../../infra/haiku-name';
+import { pseudoRandomBytesAsync } from '../../../lib/utils';
+
+const INVALID_NEWLINE_REGEX = /\r|\n/;
+export const isDeviceNameValid = (name: string) => {
+	return !INVALID_NEWLINE_REGEX.test(name);
+};
+
+hooks.addPureHook('POST', 'resin', 'device', {
+	POSTPARSE: async ({ request }) => {
+		// Check for extra whitespace characters
+		if (
+			request.values.device_name != null &&
+			!isDeviceNameValid(request.values.device_name)
+		) {
+			throw new errors.BadRequestError(
+				'Device name cannot contain any newline characters.',
+			);
+		}
+
+		request.values.device_name =
+			request.values.device_name || haikuName.generate();
+		request.values.uuid =
+			request.values.uuid || (await pseudoRandomBytesAsync(31)).toString('hex');
+
+		if (!/^[a-f0-9]{32}([a-f0-9]{30})?$/.test(request.values.uuid)) {
+			throw new errors.BadRequestError(
+				'Device UUID must be a 32 or 62 character long lower case hex string.',
+			);
+		}
+	},
+});
+
+hooks.addPureHook('PATCH', 'resin', 'device', {
+	POSTPARSE: (args) => {
+		const { request } = args;
+
+		// Check for extra whitespace characters
+		if (
+			request.values.device_name != null &&
+			!isDeviceNameValid(request.values.device_name)
+		) {
+			throw new errors.BadRequestError(
+				'Device name cannot contain any newline characters.',
+			);
+		}
+		// Parse and set `os_variant` from `os_version` if not explicitly given
+		if (
+			request.values.os_version != null &&
+			request.values.os_variant == null
+		) {
+			const match = /^.*\((.+)\)$/.exec(request.values.os_version);
+			if (match != null) {
+				request.values.os_variant = match[1];
+			} else {
+				request.values.os_variant = null;
+			}
+		}
+
+		// When moving application make sure to set the build to null, unless a specific new
+		// build has been targeted, instead of pointing to a build of the wrong application
+		if (
+			request.values.belongs_to__application != null &&
+			request.values.should_be_running__release === undefined
+		) {
+			request.values.should_be_running__release = null;
+		}
+
+		if (request.values.is_connected_to_vpn != null) {
+			request.values.is_online = request.values.is_connected_to_vpn;
+			request.values.last_vpn_event = new Date();
+		}
+
+		if (request.values.is_online != null) {
+			request.values.last_connectivity_event = new Date();
+		}
+	},
+});
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -5,6 +5,7 @@ import '../features/applications/hooks';
 import '../features/ci-cd/hooks';
 import '../features/cascade-delete/hooks';
 import '../features/dependent-devices/hooks';
+import '../features/devices/hooks';
 import '../features/device-types/hooks';
 import '../features/tags/hooks';
 import '../features/vars-schema/hooks';
