71e7ef3d2e3618f4857b7905d478ef9eeafad180
Stevche Radevski <sradevski@live.com>
2020-09-08 10:58:31 +0200

    Add a "cpu architecture" resource
    The device type resource will have a foreign key to an
    architecture, which for now only has a slug.
    
    Change-type: minor
    Signed-off-by: Stevche Radevski <stevche@balena.io>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -33,6 +33,7 @@ export const ROLES: {
 		'resin.api_key.read?is_of__actor eq @__ACTOR_ID',
 		'resin.application.all',
 		'resin.device_type.read',
+		'resin.cpu_architecture.read',
 		'resin.application_config_variable.all',
 		'resin.application_environment_variable.all',
 		'resin.application_tag.all',
