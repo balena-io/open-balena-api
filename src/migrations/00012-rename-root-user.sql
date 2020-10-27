dcb555b71ea81292e99b248201303ebbad5e3559
Akis Kesoglou <akiskesoglou@gmail.com>
2020-10-27 11:44:51 +0200

    Fixup migrations

--- src/migrations/00012-rename-root-user.sql
+++ src/migrations/00012-rename-root-user.sql
@@ -1,1 +1,11 @@
+CREATE TABLE IF NOT EXISTS "user-has-public key" (
+	"created at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+,	"user" INTEGER NOT NULL
+,	"public key" TEXT NOT NULL
+,	"id" SERIAL NOT NULL PRIMARY KEY
+,	"title" VARCHAR(255) NOT NULL
+,	FOREIGN KEY ("user") REFERENCES "user" ("id")
+,	UNIQUE("user", "public key")
+);
+
 UPDATE "user" SET username='admin' WHERE username='root';
