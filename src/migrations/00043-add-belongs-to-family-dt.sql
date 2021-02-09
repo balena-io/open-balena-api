302dc254999bc21c8ecb35a5d82c794b868fd628
Stevche Radevski <sradevski@live.com>
2021-02-09 12:44:00 +0100

    Fix missing field in migration for device family
    Change-type: patch
    Signed-off-by: Stevche Radevski <stevche@balena.io>

--- src/migrations/00043-add-belongs-to-family-dt.sql
+++ src/migrations/00043-add-belongs-to-family-dt.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE IF NOT EXISTS "device manufacturer" (
     "created at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modified at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -12,9 +13,10 @@ CREATE TABLE IF NOT EXISTS "device family" (
     "id" SERIAL NOT NULL PRIMARY KEY,
     "slug" VARCHAR(255) NOT NULL UNIQUE,
     "name" VARCHAR(255) NOT NULL,
-    FOREIGN KEY ("is manufactured by-device_manufacturer") REFERENCES "device manufacturer" ("id")
+    "is manufactured by-device manufacturer" INTEGER NOT NULL,
+    FOREIGN KEY ("is manufactured by-device manufacturer") REFERENCES "device manufacturer" ("id")
 );
 
-ALTER TABLE "device type" 
+ALTER TABLE "device type"
 ADD COLUMN IF NOT EXISTS "belongs to-device family" INTEGER NULL,
 ADD CONSTRAINT "device type_belongs to-device family_fkey" FOREIGN KEY ("belongs to-device family") REFERENCES "device family" ("id");
