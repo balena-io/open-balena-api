3819c76d11f1fe6f7b46faf91889cd0e2c82ef54
Matthew McGinn <matthew@balena.io>
2021-02-12 11:47:31 -0500

    Revert "WIP - resolving PR feedback"
    This reverts commit 58e15498968599b36bed966ef5e8044a97e68315.

--- src/features/devices/hooks/should-be-managed-by--release.ts
+++ src/features/devices/hooks/should-be-managed-by--release.ts
@@ -1,4 +1,4 @@
-import { hooks, sbvrUtils } from '@balena/pinejs';
+import { hooks } from '@balena/pinejs';
 import { getAffectedIds } from '@balena/pinejs/out/sbvr-api/sbvr-utils';
 
 hooks.addPureHook('PATCH', 'resin', 'device', {
@@ -13,24 +13,10 @@ hooks.addPureHook('PATCH', 'resin', 'device', {
 			return;
 		}
 
-		/**
-		 * These device(s) should have a valid release assigned based on their current
-		 * running supervisor version, if we have one...
-		 */
-
-		const [device] = await args.api.get({
+		await args.api.get({
 			resource: 'device',
 			options: {
 				$select: ['supervisor_version'],
-				$expand: {
-					is_for__device_type: {
-						$expand: {
-							is_of__cpu_architecture: {
-								$select: 'id',
-							},
-						},
-					},
-				},
 				$filter: {
 					id: {
 						$in: deviceIds,
@@ -40,72 +26,9 @@ hooks.addPureHook('PATCH', 'resin', 'device', {
 			},
 		});
 
-		if (!device) {
-			return;
-		}
-
-		const [supervisorRelease] = await getSupervisorReleaseResource(
-			args.api,
-			device.supervisor_version,
-			device.is_for__device_type.is_of__cpu_architecture.id,
-		);
-
-		if (!supervisorRelease) {
-			return;
-		}
-
-		request.values.should_be_managed_by__release = supervisorRelease.id;
+		/**
+		 * These device(s) should have a valid release assigned based on their current
+		 * running supervisor version, if we have one...
+		 */
 	},
 });
-
-async function getSupervisorReleaseResource(
-	api: sbvrUtils.PinejsClient,
-	supervisorVersion: string,
-	archId: string,
-) {
-	return await api.get({
-		resource: 'release',
-		options: {
-			$select: ['id', 'release_version'],
-			$expand: {
-				belongs_to__application: {
-					$select: ['is_for__device_type'],
-				},
-			},
-			$filter: {
-				release_version: `v${supervisorVersion}`,
-				status: 'success',
-				belongs_to__application: {
-					$any: {
-						$alias: 'a',
-						$expr: {
-							a: {
-								is_public: true,
-								is_host: false,
-								is_for__device_type: {
-									$any: {
-										$alias: 'dt',
-										$expr: {
-											dt: {
-												is_of__cpu_architecture: {
-													$any: {
-														$alias: 'c',
-														$expr: {
-															c: {
-																id: archId,
-															},
-														},
-													},
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	});
-}
