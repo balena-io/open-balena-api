7c416b41d8921d03dea0ee9a49bbc7d5a2ca5d89
Pagan Gazzard <page@balena.io>
2021-03-10 18:16:42 +0000

    Add username normalization
    Change-type: patch

--- src/features/auth/hooks/validate-username-email.ts
+++ src/features/auth/hooks/validate-username-email.ts
@@ -1,5 +1,6 @@
 import { hooks, errors } from '@balena/pinejs';
 import { findUser } from '../../../infra/auth/auth';
+import { normalizeHandle } from '../handles';
 
 const { ConflictError } = errors;
 
@@ -8,6 +9,7 @@ const USERNAME_BLACKLIST = ['root'];
 for (const method of ['POST', 'PATCH'] as const) {
 	hooks.addPureHook(method, 'resin', 'user', {
 		POSTPARSE: async ({ request, tx }) => {
+			request.values.username = normalizeHandle(request.values.username);
 			if (USERNAME_BLACKLIST.includes(request.values.username)) {
 				throw new ConflictError('This username is blacklisted');
 			}
