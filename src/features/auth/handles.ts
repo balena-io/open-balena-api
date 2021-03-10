1b15c3e9192b8af05f13c4fe6c0572163fb19ad9
Pagan Gazzard <page@balena.io>
2021-03-10 19:43:35 +0000

    Add organization handle validation
    Change-type: patch

--- src/features/auth/handles.ts
+++ src/features/auth/handles.ts
@@ -1,5 +1,20 @@
+import { errors } from '@balena/pinejs';
+
+const { BadRequestError } = errors;
+
 const normalizeReplacements = /[^\w]+/g;
 
 export const normalizeHandle = (handle: string): string => {
 	return handle.replace(normalizeReplacements, '_').toLowerCase();
+};
+
+export const validateHandle = (handle: string) => {
+	if (normalizeReplacements.test(handle)) {
+		throw new BadRequestError(
+			'Handles can only contain alphanumeric characters and underscores',
+		);
+	}
+	if (handle !== handle.toLowerCase()) {
+		throw new BadRequestError('Handles must be lowercase');
+	}
 };
