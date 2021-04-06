85f72890c99d3e86f91bbc4d3fc0a1184650f60f
Matthew McGinn <matthew@balena.io>
2021-04-06 13:57:16 -0400

    hostapps: basic implementation of target hostapp release
    including a hook to set devices to a hostapp release at provision time
    
    Change-type: minor
    Signed-off-by: Matthew McGinn <matthew@balena.io>

---
+++ src/features/hostapp/hooks/target-hostapp.ts
@@ -0,0 +1,296 @@
+import {
+	sbvrUtils,
+	hooks,
+	permissions,
+	errors as pinejsErrors,
+} from '@balena/pinejs';
+import * as semver from 'balena-semver';
+import * as _ from 'lodash';
+import { Device, ReleaseTag, PickDeferred } from '../../../balena-model';
+const { BadRequestError } = pinejsErrors;
+
+hooks.addPureHook('PATCH', 'resin', 'device', {
+	/**
+	 * Disallow hostapp downgrades, using the related release resource
+	 */
+	async PRERUN(args) {
+		if (args.request.values.should_be_operated_by__release != null) {
+			// First try to coerce the value to an integer for
+			// moving forward
+			args.request.custom.hostappRelease = parseInt(
+				args.request.values.should_be_operated_by__release,
+				10,
+			);
+			// But let's check we actually got a value
+			// representing an integer
+			if (!Number.isInteger(args.request.custom.hostappRelease)) {
+				throw new BadRequestError('Expected an ID for the hostapp release');
+			}
+
+			// Ensure that we don't ever downgrade the hostapp
+			// from it's current version
+			const ids = await sbvrUtils.getAffectedIds(args);
+			await checkHostappReleaseUpgrades(
+				args.api,
+				ids,
+				args.request.custom.hostappRelease,
+			);
+		}
+	},
+});
+
+hooks.addPureHook('PATCH', 'resin', 'device', {
+	/**
+	 * When a device checks in with it's initial OS version, set the corresponding should_be_operated_by__release resource
+	 * using its current reported version.
+	 */
+	async PRERUN(args) {
+		if (args.request.values.os_version != null) {
+			const ids = await sbvrUtils.getAffectedIds(args);
+			await setOSReleaseResource(
+				args.api,
+				ids,
+				args.request.values.os_version,
+				args.request.values.os_variant,
+			);
+		}
+	},
+});
+
+async function setOSReleaseResource(
+	api: sbvrUtils.PinejsClient,
+	deviceIds: number[],
+	osVersion: string,
+	osVariant: string,
+) {
+	if (deviceIds.length === 0) {
+		return;
+	}
+	const devices = (await api.get({
+		resource: 'device',
+		options: {
+			// if the device already has an os_version, just bail.
+			$filter: {
+				id: { $in: deviceIds },
+				os_version: null,
+			},
+			$select: ['id', 'is_of__device_type'],
+		},
+	})) as Array<PickDeferred<Device, 'id' | 'is_of__device_type'>>;
+
+	if (devices.length === 0) {
+		return;
+	}
+
+	const devicesByDeviceType = _.groupBy(devices, (d) => {
+		return d.is_of__device_type.__id;
+	});
+
+	if (Object.keys(devicesByDeviceType).length === 0) {
+		return;
+	}
+
+	const rootApi = api.clone({
+		passthrough: {
+			req: permissions.root,
+		},
+	});
+
+	return Promise.all(
+		_.map(devicesByDeviceType, async (affectedDevices, deviceType) => {
+			const affectedDeviceIds = affectedDevices.map((d) => d.id);
+
+			const [osRelease] = await getOSReleaseResource(
+				api,
+				osVersion,
+				osVariant,
+				deviceType,
+			);
+
+			if (osRelease == null) {
+				return;
+			}
+
+			await rootApi.patch({
+				resource: 'device',
+				options: {
+					$filter: {
+						id: { $in: affectedDeviceIds },
+					},
+				},
+				body: {
+					should_be_operated_by__release: osRelease.id,
+				},
+			});
+		}),
+	);
+}
+
+async function getOSReleaseResource(
+	api: sbvrUtils.PinejsClient,
+	osVersion: string,
+	osVariant: string,
+	deviceTypeId: string,
+) {
+	return await api.get({
+		resource: 'release',
+		options: {
+			$select: ['id', 'belongs_to__application'],
+			$filter: {
+				// TODO: better to (eventually) use release version, once that's fully supported
+				// https://www.flowdock.com/app/rulemotion/resin-tech/threads/Ao4qzbDh8Z4Pgq_xF99Ld5fME35
+				status: 'success',
+				belongs_to__application: {
+					$any: {
+						$alias: 'a',
+						$expr: {
+							a: {
+								is_for__device_type: deviceTypeId,
+								is_host: true,
+							},
+						},
+					},
+				},
+				$and: [
+					{
+						release_tag: {
+							$any: {
+								$alias: 'rt',
+								$expr: {
+									rt: {
+										value: normalizeOsVersion(osVersion),
+										tag_key: 'version',
+									},
+								},
+							},
+						},
+					},
+					{
+						release_tag: {
+							$any: {
+								$alias: 'rt',
+								$expr: {
+									rt: {
+										value: normalizeVariant(osVariant),
+										tag_key: 'variant',
+									},
+								},
+							},
+						},
+					},
+				],
+			},
+		},
+	});
+}
+
+function normalizeVariant(variant: string) {
+	switch (variant) {
+		case 'prod':
+			return 'production';
+		case 'dev':
+			return 'development';
+		default:
+			return variant;
+	}
+}
+
+function normalizeOsVersion(osVersion: string) {
+	// Remove "Resin OS" and "Balena OS" text
+	return (
+		osVersion
+			.replace(/(resin|balena)\s*os\s*/gi, '')
+			// Remove optional versioning, eg "(prod)", "(dev)"
+			.replace(/\s+\(\w+\)$/, '')
+			// Remove "v" prefix
+			.replace(/^v/, '')
+	);
+}
+
+async function checkHostappReleaseUpgrades(
+	api: sbvrUtils.PinejsClient,
+	deviceIds: number[],
+	newHostappReleaseId: number,
+) {
+	if (deviceIds.length === 0) {
+		return;
+	}
+	const nullHostappCount = await api.get({
+		resource: 'device',
+		options: {
+			$count: { $filter: { id: { $in: deviceIds }, os_version: null } },
+		},
+	});
+
+	if (nullHostappCount === deviceIds.length) {
+		return;
+	}
+
+	// In order to prevent blocking devices from provisioning, we allow devices to come online, report any version and
+	// we tag it accordingly. However we do not allow devices to _upgrade_ to invalidated releases, so we filter those
+	// out here.
+	const newHostappRelease = await api.get({
+		resource: 'release',
+		id: newHostappReleaseId,
+		options: {
+			$expand: {
+				release_tag: { $filter: { tag_key: 'version' }, $select: ['value'] },
+			},
+			$filter: {
+				// we shouldn't be able to upgrade to an invalid release, but we can provision to one (so this isn't an
+				// SBVR rule)
+				is_invalidated: false,
+				status: 'success',
+			},
+		},
+	});
+
+	if (newHostappRelease == null) {
+		throw new BadRequestError(
+			`Could not find a hostapp release with this ID ${newHostappReleaseId}`,
+		);
+	}
+
+	// TODO: this validation should eventually use release_version, not tags
+	const newHostappVersion = newHostappRelease.release_tag[0].value;
+
+	const releases = (await api.get({
+		resource: 'release_tag',
+		options: {
+			$select: ['value'],
+			$filter: {
+				tag_key: 'version',
+				release: {
+					$any: {
+						$alias: 'r',
+						$expr: {
+							r: {
+								should_operate__device: {
+									$any: {
+										$alias: 'd',
+										$expr: {
+											d: {
+												id: {
+													$in: deviceIds,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	})) as Array<Pick<ReleaseTag, 'value'>>;
+
+	for (const release of releases) {
+		const oldVersion = release.value;
+		if (semver.lt(newHostappVersion, oldVersion)) {
+			throw new BadRequestError(
+				`Attempt to downgrade hostapp, which is not allowed`,
+			);
+		}
+	}
+}
---
+++ test/15_target-hostapp.ts
@@ -0,0 +1,198 @@
+import * as _ from 'lodash';
+import * as fixtures from './test-lib/fixtures';
+import * as fakeDevice from './test-lib/fake-device';
+import { expect } from './test-lib/chai';
+
+import { supertest, UserObjectParam } from './test-lib/supertest';
+import { version } from './test-lib/versions';
+
+describe('target hostapps', () => {
+	let fx: fixtures.Fixtures;
+	let admin: UserObjectParam;
+	let applicationId: number;
+	let device: fakeDevice.Device;
+	let esrDevice: fakeDevice.Device;
+	let noMatchDevice: fakeDevice.Device;
+	let invalidatedReleaseDevice: fakeDevice.Device;
+	let prodNucHostappReleaseId: number;
+	let raspberryPiHostappReleaseId: number;
+	let upgradeReleaseId: number;
+	let esrHostappReleaseId: number;
+	let invalidatedReleaseId: number;
+
+	before(async () => {
+		fx = await fixtures.load('15-target-hostapps');
+		admin = fx.users.admin;
+		applicationId = fx.applications['user-app1'].id;
+		device = await fakeDevice.provisionDevice(admin, applicationId);
+		esrDevice = await fakeDevice.provisionDevice(admin, applicationId);
+		noMatchDevice = await fakeDevice.provisionDevice(admin, applicationId);
+		invalidatedReleaseDevice = await fakeDevice.provisionDevice(
+			admin,
+			applicationId,
+		);
+		prodNucHostappReleaseId = fx.releases.release0.id;
+		raspberryPiHostappReleaseId = fx.releases.release1.id;
+		upgradeReleaseId = fx.releases.release2.id;
+		esrHostappReleaseId = fx.releases.release3.id;
+		invalidatedReleaseId = fx.releases.release5.id;
+	});
+
+	after(async () => {
+		await fixtures.clean({
+			devices: [device, esrDevice, noMatchDevice, invalidatedReleaseDevice],
+		});
+		await fixtures.clean(fx);
+	});
+
+	it('should provision with a linked hostapp', async () => {
+		const devicePatchBody = {
+			local: {
+				is_online: true,
+				os_version: 'balenaOS 2.50.0+rev1',
+				os_variant: 'prod',
+			},
+		};
+
+		await device.patchStateV2(devicePatchBody);
+		const { body } = await supertest(admin)
+			.get(
+				`/${version}/device(${device.id})?$select=should_be_operated_by__release`,
+			)
+			.expect(200);
+		expect(body.d[0]).to.not.be.undefined;
+		expect(body.d[0]['should_be_operated_by__release']).to.be.not.null;
+		expect(body.d[0]['should_be_operated_by__release'].__id).to.equal(
+			prodNucHostappReleaseId,
+		);
+	});
+
+	it('should provision with a linked ESR hostapp', async () => {
+		const devicePatchBody = {
+			local: {
+				is_online: true,
+				os_version: 'balenaOS 2021.01.0',
+				os_variant: 'prod',
+			},
+		};
+
+		await esrDevice.patchStateV2(devicePatchBody);
+		const { body } = await supertest(admin)
+			.get(
+				`/${version}/device(${esrDevice.id})?$select=should_be_operated_by__release`,
+			)
+			.expect(200);
+		expect(body.d[0]).to.not.be.undefined;
+		expect(body.d[0]['should_be_operated_by__release']).to.be.not.null;
+	});
+
+	it('should fail to PATCH intel-nuc device to raspberrypi3 hostapp', async () => {
+		await supertest(admin)
+			.patch(`/${version}/device(${device.id})`)
+			.send({ should_be_operated_by__release: raspberryPiHostappReleaseId })
+			.expect(
+				400,
+				'"It is necessary that each release that should operate a device, belongs to an application that is host and is for a device type that describes the device."',
+			);
+	});
+
+	it('should succeed in PATCHing device to greater version', async () => {
+		await supertest(admin)
+			.patch(`/${version}/device(${device.id})`)
+			.send({ should_be_operated_by__release: upgradeReleaseId })
+			.expect(200);
+		const { body } = await supertest(admin).get(
+			`/${version}/device(${device.id})?$select=should_be_operated_by__release`,
+		);
+		expect(body.d[0]).to.not.be.undefined;
+		expect(body.d[0]['should_be_operated_by__release'].__id).to.equal(
+			upgradeReleaseId,
+		);
+	});
+
+	it('should fail to downgrade', async () => {
+		await supertest(admin)
+			.patch(`/${version}/device(${device.id})`)
+			.send({ should_be_operated_by__release: fx.releases.release0.id })
+			.expect(400, '"Attempt to downgrade hostapp, which is not allowed"');
+	});
+
+	it('should succeed in PATCHing device to ESR release', async () => {
+		await supertest(admin)
+			.patch(`/${version}/device(${device.id})`)
+			.send({ should_be_operated_by__release: esrHostappReleaseId })
+			.expect(200);
+		const { body } = await supertest(admin).get(
+			`/${version}/device(${device.id})?$select=should_be_operated_by__release`,
+		);
+		expect(body.d[0]).to.not.be.undefined;
+		expect(body.d[0]['should_be_operated_by__release'].__id).to.equal(
+			esrHostappReleaseId,
+		);
+	});
+
+	it('should still provision with a nonexistent hostapp', async () => {
+		const devicePatchBody = {
+			local: {
+				is_online: true,
+				os_version: 'balenaOS 2999.01.0',
+				os_variant: 'prod',
+			},
+		};
+
+		await noMatchDevice.patchStateV2(devicePatchBody);
+		const { body } = await supertest(admin)
+			.get(
+				`/${version}/device(${noMatchDevice.id})?$select=should_be_operated_by__release`,
+			)
+			.expect(200);
+		expect(body.d[0]).to.not.be.undefined;
+		expect(body.d[0]['should_be_operated_by__release']).to.be.null;
+		expect(body.d[0]['os_version']).to.be.not.null;
+		expect(body.d[0]['os_variant']).to.be.not.null;
+	});
+
+	it('should provision with an invalidated hostapp release', async () => {
+		const devicePatchBody = {
+			local: {
+				is_online: true,
+				os_version: 'balenaOS 2.52.0+rev1',
+				os_variant: 'prod',
+			},
+		};
+
+		await invalidatedReleaseDevice.patchStateV2(devicePatchBody);
+		const { body } = await supertest(admin)
+			.get(
+				`/${version}/device(${invalidatedReleaseDevice.id})?$select=should_be_operated_by__release`,
+			)
+			.expect(200);
+		expect(body.d[0]).to.not.be.undefined;
+		expect(body.d[0]['should_be_operated_by__release'].__id).to.equal(
+			invalidatedReleaseId,
+		);
+		expect(body.d[0]['os_version']).to.be.not.null;
+		expect(body.d[0]['os_variant']).to.be.not.null;
+	});
+
+	it('should be able to invalidate a release with devices attached', async () => {
+		await supertest(admin)
+			.patch(`/${version}/release(${prodNucHostappReleaseId})`)
+			.send({ is_invalidated: true })
+			.expect(200);
+		const { body } = await supertest(admin)
+			.get(`/${version}/release(${prodNucHostappReleaseId})`)
+			.expect(200);
+		expect(body.d[0].is_invalidated).to.be.true;
+	});
+
+	it('should not be able to upgrade to an invalidated release', async () => {
+		await supertest(admin)
+			.patch(`/${version}/device(${device.id})`)
+			.send({ should_be_operated_by__release: prodNucHostappReleaseId })
+			.expect(
+				400,
+				`"Could not find a hostapp release with this ID ${prodNucHostappReleaseId}"`,
+			);
+	});
+});
