16e13d78ddf511c60f43af1fb2c4881117c1ba49
Dan Goodman <danthegoodman@icloud.com>
2021-04-21 11:50:28 -0400

    Add trivy
    Change-type: minor

--- automation/test.sh
+++ automation/test.sh
@@ -16,3 +16,14 @@ api_id=$(runapi $IMAGE_NAME $db_id $redis_id $loki_id)
 setup $api_id
 
 docker exec $api_id /bin/sh -c 'npx mocha'
+
+# Run Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+trivy image -f table $IMAGE_NAME
+ls # see what the file is called
+
+# Run Grype
