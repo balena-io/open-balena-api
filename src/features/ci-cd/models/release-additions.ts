d8ccbd3755eaba98c071700f71b8a2b4411b3900
Thodoris Greasidis <thgreasi@gmail.com>
2021-08-12 17:49:03 +0300

    Add release.version JSON field
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- src/features/ci-cd/models/release-additions.ts
+++ src/features/ci-cd/models/release-additions.ts
@@ -1,24 +1,105 @@
-import type { AbstractSqlModel } from '@balena/abstract-sql-compiler';
+import type {
+	AbstractSqlModel,
+	CastNode,
+	ReferencedFieldNode,
+	ExistsNode,
+} from '@balena/abstract-sql-compiler';
+import { oneLineTrimSqlConcat } from '../../../abstract-sql-utils';
 
 export const addToModel = (abstractSql: AbstractSqlModel) => {
+	const [revisionField, majorField, minorField, patchField] = [
+		'revision',
+		'semver major',
+		'semver minor',
+		'semver patch',
+	].map((field): ReferencedFieldNode => ['ReferencedField', 'release', field]);
+
+	const isFinal: ExistsNode = ['Exists', revisionField];
 	abstractSql.tables['release'].fields.push({
 		fieldName: 'is final',
 		dataType: 'Boolean',
 		required: true,
-		computed: ['Exists', ['ReferencedField', 'release', 'revision']],
+		computed: isFinal,
 	});
 
+	const semverField = oneLineTrimSqlConcat`${majorField}.${minorField}.${patchField}`;
 	abstractSql.tables['release'].fields.push({
 		fieldName: 'semver',
 		dataType: 'Short Text',
 		required: true,
-		computed: [
-			'Concatenate',
-			['ReferencedField', 'release', 'semver major'],
-			['EmbeddedText', '.'],
-			['ReferencedField', 'release', 'semver minor'],
-			['EmbeddedText', '.'],
-			['ReferencedField', 'release', 'semver patch'],
+		computed: semverField,
+	});
+
+	const createdAtTimestampNode: CastNode = [
+		'Cast',
+		[
+			'Floor',
+			[
+				'Multiply',
+				['Totalseconds', ['ReferencedField', 'release', 'created at']],
+				['Number', 1000],
+			],
 		],
+		'Text',
+	];
+
+	abstractSql.tables['release'].fields.push({
+		fieldName: 'version',
+		dataType: 'JSON',
+		required: true,
+		computed: oneLineTrimSqlConcat`{
+			"raw": "${semverField}${[
+			'Cast',
+			[
+				'Case',
+				[
+					'When',
+					['NotExists', revisionField],
+					oneLineTrimSqlConcat`-${createdAtTimestampNode}`,
+				],
+				[
+					'When',
+					['GreaterThan', revisionField, ['Number', 0]],
+					oneLineTrimSqlConcat`+rev${revisionField}`,
+				],
+				['Else', ['EmbeddedText', '']],
+			],
+			'Text',
+		]}",
+			"major": ${majorField},
+			"minor": ${minorField},
+			"patch": ${patchField},
+			"prerelease": [${[
+				'Cast',
+				[
+					'Case',
+					['When', isFinal, ['EmbeddedText', '']],
+					['Else', createdAtTimestampNode],
+				],
+				'Text',
+			]}],
+			"build": [${[
+				'Cast',
+				[
+					'Case',
+					[
+						'When',
+						['GreaterThan', revisionField, ['Number', 0]],
+						oneLineTrimSqlConcat`"rev${revisionField}"`,
+					],
+					['Else', ['EmbeddedText', '']],
+				],
+				'Text',
+			]}],
+			"version": "${semverField}${[
+			'Cast',
+			[
+				'Case',
+				['When', isFinal, ['EmbeddedText', '']],
+				['Else', oneLineTrimSqlConcat`-${createdAtTimestampNode}`],
+			],
+			'Text',
+		]}"
+		}`,
 	});
 };
