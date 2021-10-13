5c84c2e5c60f09ee2a20fbea17c56ee2c64ee739
Pagan Gazzard <page@balena.io>
2021-10-13 20:08:11 +0100

    Change image install index to only look at download progress existence
    Change-type: patch

--- src/migrations/00058-add-index.sql
+++ src/migrations/00058-add-index.sql
@@ -1,3 +1,1 @@
--- Optimize the overall status computed fact type
-CREATE INDEX IF NOT EXISTS "image_install_status_dl_progress_device_idx"
-ON "image install" ("status", "download progress", "device");
+-- Removed the migration as we replace it in 00060 and this can avoid unnecessarily creating it just to immediately remove it
