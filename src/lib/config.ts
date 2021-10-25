7a8c44ae7a39a2cf5e46f361e70ad28ecbe05943
Pagan Gazzard <page@balena.io>
2021-10-25 14:29:35 +0100

    Allow configuring the trust proxy setting via an env var
    Change-type: patch

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -246,3 +246,22 @@ export const VPN_AUTH_CACHE_TIMEOUT = intVar(
 	'VPN_AUTH_CACHE_TIMEOUT',
 	5 * MINUTES,
 );
+
+const { TRUST_PROXY: trustProxy = 'true' } = process.env;
+let trustProxyValue;
+if (trustProxy === 'true') {
+	// If it's 'true' enable it
+	trustProxyValue = true;
+} else if (trustProxy.includes('.') || trustProxy.includes(':')) {
+	// If it looks like an ip use as-is
+	trustProxyValue = trustProxy;
+} else {
+	const trustProxyNum = parseInt(trustProxy, 10);
+	if (Number.isFinite(trustProxyNum)) {
+		// If it's a number use the number
+		trustProxyValue = trustProxyNum;
+	} else {
+		throw new Error(`Invalid value for 'TRUST_PROXY' of '${trustProxy}'`);
+	}
+}
+export const TRUST_PROXY = trustProxyValue;
