4d9a10dbe4a5310bf807479812a0171db1bbdf2b
Thodoris Greasidis <thgreasi@gmail.com>
2021-12-08 16:18:03 +0200

    Limit changing the app DT only to ones of the same cpu architecture
    Change-type: minor
    See: https://github.com/balena-io/balena-api/issues/3463
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- src/features/applications/hooks/index.ts
+++ src/features/applications/hooks/index.ts
@@ -1,4 +1,5 @@
 import './backwards-compatible-delete-error';
+import './change-default-device-type';
 import './defaults';
 import './check-device-releases-before-delete';
 import './unpin-release-before-delete';
