9442564625618721a86b265bb38fa7ce18bb77f3
Pagan Gazzard <page@balena.io>
2022-04-26 12:10:58 +0100

    Use a multi-level cache for device types info
    This also lets the cache handle the automatic refreshing rather than
    having custom code for it
    
    Change-type: patch

--- src/features/device-types/device-types-list.ts
+++ src/features/device-types/device-types-list.ts
@@ -10,15 +10,13 @@ import { captureException } from '../../infra/error-handling';
 
 import { getDeviceTypeJson, getLogoUrl } from './build-info-facade';
 import { getImageKey, IMAGE_STORAGE_PREFIX, listFolders } from './storage';
-import { withRetries } from '../../lib/utils';
-import { setTimeout } from 'timers/promises';
+import { multiCacheMemoizee } from '../../infra/cache';
+import { DEVICE_TYPES_CACHE_TIMEOUT } from '../../lib/config';
 
 export interface DeviceTypeInfo {
 	latest: DeviceTypeJson;
 	versions: string[];
 }
-
-const DEVICE_TYPES_CACHE_EXPIRATION = 5 * 60 * 1000; // 5 mins
 
 function sortBuildIds(ids: string[]): string[] {
 	return arraySort(
@@ -54,90 +52,51 @@ const getFirstValidBuild = async (
 	}
 };
 
-async function fetchDeviceTypes(): Promise<Dictionary<DeviceTypeInfo>> {
-	const result: Dictionary<DeviceTypeInfo> = {};
-	const slugs = await listFolders(IMAGE_STORAGE_PREFIX);
-	await Promise.all(
-		slugs.map(async (slug) => {
-			try {
-				const builds = await listFolders(getImageKey(slug));
-				if (_.isEmpty(builds)) {
-					return;
+export const getDeviceTypes = multiCacheMemoizee(
+	async (): Promise<Dictionary<DeviceTypeInfo>> => {
+		const result: Dictionary<DeviceTypeInfo> = {};
+		const slugs = await listFolders(IMAGE_STORAGE_PREFIX);
+		await Promise.all(
+			slugs.map(async (slug) => {
+				try {
+					const builds = await listFolders(getImageKey(slug));
+					if (_.isEmpty(builds)) {
+						return;
+					}
+
+					const sortedBuilds = sortBuildIds(builds);
+					const latestDeviceType = await getFirstValidBuild(slug, sortedBuilds);
+					if (!latestDeviceType) {
+						return;
+					}
+
+					result[slug] = {
+						versions: builds,
+						latest: latestDeviceType,
+					};
+
+					_.forEach(latestDeviceType.aliases, (alias) => {
+						result[alias] = result[slug];
+					});
+				} catch (err) {
+					captureException(
+						err,
+						`Failed to find a valid build for device type ${slug}`,
+					);
 				}
+			}),
+		);
 
-				const sortedBuilds = sortBuildIds(builds);
-				const latestDeviceType = await getFirstValidBuild(slug, sortedBuilds);
-				if (!latestDeviceType) {
-					return;
-				}
-
-				result[slug] = {
-					versions: builds,
-					latest: latestDeviceType,
-				};
-
-				_.forEach(latestDeviceType.aliases, (alias) => {
-					result[alias] = result[slug];
-				});
-			} catch (err) {
-				captureException(
-					err,
-					`Failed to find a valid build for device type ${slug}`,
-				);
-			}
-		}),
-	);
-
-	if (_.isEmpty(result) && !_.isEmpty(slugs)) {
-		throw new InternalRequestError('Could not retrieve any device type');
-	}
-	return result;
-}
-
-let deviceTypesCache:
-	| Promise<Dictionary<DeviceTypeInfo>>
-	| Dictionary<DeviceTypeInfo>
-	| undefined;
-
-async function scheduleFetchDeviceTypes() {
-	try {
-		await setTimeout(DEVICE_TYPES_CACHE_EXPIRATION);
-		await fetchDeviceTypesAndReschedule();
-	} catch (err) {
-		captureException(err, 'Failed to re-fetch device types');
-	}
-}
-
-async function fetchDeviceTypesAndReschedule(): Promise<
-	Dictionary<DeviceTypeInfo>
-> {
-	try {
-		const promise = withRetries(fetchDeviceTypes);
-
-		// if the cache is still empty, use this promise so that
-		// we do not start a second set of requests to s3
-		// in case another api request comes before the first completes
-		if (!deviceTypesCache) {
-			deviceTypesCache = promise;
+		if (_.isEmpty(result) && !_.isEmpty(slugs)) {
+			throw new InternalRequestError('Could not retrieve any device type');
 		}
-
-		try {
-			deviceTypesCache = await promise;
-			return deviceTypesCache;
-		} catch (err) {
-			if (deviceTypesCache === promise) {
-				deviceTypesCache = undefined;
-			}
-			captureException(err, 'Failed to get device types');
-			throw err;
-		}
-	} finally {
-		// schedule a re-run to update the local cache - do not wait for it
-		scheduleFetchDeviceTypes();
-	}
-}
-
-export async function getDeviceTypes(): Promise<Dictionary<DeviceTypeInfo>> {
-	// Always return the local cache if populated
-	return await (deviceTypesCache ?? fetchDeviceTypesAndReschedule());
-}
+		return result;
+	},
+	{
+		cacheKey: 'fetchDeviceTypes',
+		promise: true,
+		primitive: true,
+		preFetch: 0.1,
+		maxAge: DEVICE_TYPES_CACHE_TIMEOUT,
+	},
+);
