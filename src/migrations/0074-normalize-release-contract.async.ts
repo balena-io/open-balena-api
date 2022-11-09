52a7aba3a9a29cff95af4bee3d5cc4d5d7b8a7b6
Josh Bowling <josh@balena.io>
2022-11-09 05:10:36 +0000

    Migration: Normalize release contract fields
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@monarci.com>

---
+++ src/migrations/0074-normalize-release-contract.async.ts
@@ -0,0 +1,21 @@
+/**
+ * Update "release"."contract" fields with text that has been stringified
+ * multiple times to contain text that has only been stringified once.
+ */
+export = {
+	asyncSql: `\
+    UPDATE "release" SET "contract" = "release"."contract"::jsonb#>>'{}' WHERE id IN (
+        SELECT id FROM "release" WHERE STARTS_WITH("contract", '"') LIMIT %%ASYNC_BATCH_SIZE%% FOR UPDATE SKIP LOCKED
+    );
+    `,
+	syncSql: `\
+    UPDATE "release" SET "contract" = "release"."contract"::jsonb#>>'{}' WHERE id IN (
+        SELECT "id" FROM "release" WHERE STARTS_WITH("contract", '"') FOR UPDATE SKIP LOCKED
+    );
+    `,
+	delayMS: 10000,
+	backoffDelayMS: 4000,
+	errorThreshold: 15,
+	asyncBatchSize: 1000,
+	finalize: false,
+};
