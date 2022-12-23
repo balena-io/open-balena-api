083fce57d3894a14d70d33136f8b71e114d8198a
Pagan Gazzard <page@balena.io>
2022-12-23 13:49:22 +0000

    Add comments explaining the difference between auth middlewares

--- src/infra/auth/middleware.ts
+++ src/infra/auth/middleware.ts
@@ -4,6 +4,10 @@ import { checkSudoValidity } from './jwt';
 import { prefetchAPIKey, retrieveAPIKey } from './api-keys';
 import { getUser, reqHasPermission } from './auth';
 
+/**
+ * This checks that a user has provided credentials, they may not be fully authorized, ie they may still need to pass 2fa
+ * Note: This is specifically *user* credentials, device keys/application provisioning keys do not count
+ */
 export const authenticatedMiddleware: RequestHandler = async (
 	req,
 	res,
@@ -13,7 +17,6 @@ export const authenticatedMiddleware: RequestHandler = async (
 		await getUser(req, undefined, false);
 		if (req.creds) {
 			next();
-			return null;
 		} else {
 			res.status(401).end();
 		}
@@ -22,6 +25,10 @@ export const authenticatedMiddleware: RequestHandler = async (
 	}
 };
 
+/**
+ * This checks that a user has provided authorized credentials, ie they have passed any required 2fa checks
+ * Note: This is specifically *user* credentials, device keys/application provisioning keys do not count
+ */
 export const authorizedMiddleware: RequestHandler = async (req, res, next) => {
 	try {
 		await getUser(req, undefined);
@@ -32,17 +39,26 @@ export const authorizedMiddleware: RequestHandler = async (req, res, next) => {
 	}
 };
 
+/**
+ * This resolves any provided credentials, with no checks on the actor or 2fa status
+ */
 export const identifyMiddleware: RequestHandler = async (req, _res, next) => {
 	await getUser(req, undefined, false);
 	next();
 	return null;
 };
 
+/**
+ * This starts a prefetch of api key permissions without waiting for them to finish resolving
+ */
 export const prefetchApiKeyMiddleware: RequestHandler = (req, _res, next) => {
 	prefetchAPIKey(req, undefined);
 	next();
 };
 
+/**
+ * This resolves api key permissions, making use of the prefetch if it exists or otherwise starting a fetch from scratch
+ */
 export const apiKeyMiddleware: RequestHandler = async (req, _res, next) => {
 	try {
 		// Note: this won't reply with 401 if there's no api key
@@ -53,6 +69,12 @@ export const apiKeyMiddleware: RequestHandler = async (req, _res, next) => {
 	}
 };
 
+/**
+ * This creates a middleware that checks a specific permission is present on the request
+ *
+ * @param permission The required permission
+ * @returns The middleware to check the permission
+ */
 export const permissionRequiredMiddleware =
 	(permission: string): RequestHandler =>
 	(req, res, next) => {
@@ -64,6 +86,10 @@ export const permissionRequiredMiddleware =
 		}
 	};
 
+/**
+ * This checks that a user has authenticated recently and therefore can perform privileged operations
+ * Note: This is specifically *user* credentials, device keys/application provisioning keys do not count
+ */
 export const sudoMiddleware: RequestHandler = async (req, res, next) => {
 	try {
 		const user = await getUser(req, undefined, false);
