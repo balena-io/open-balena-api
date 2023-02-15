691b778a781880de5cc8cda4c188e1cf3525c631
Pagan Gazzard <page@balena.io>
2023-02-15 14:50:02 +0000

    Destructure hook args in the fn definition where possible
    Change-type: patch

--- src/features/dependent-devices/hooks/cascade-offline.ts
+++ src/features/dependent-devices/hooks/cascade-offline.ts
@@ -1,15 +1,15 @@
 import { hooks } from '@balena/pinejs';
 
 hooks.addPureHook('PATCH', 'resin', 'device', {
-	POSTRUN: async (args) => {
-		const affectedIds = args.request.affectedIds!;
+	POSTRUN: async ({ request, api }) => {
+		const affectedIds = request.affectedIds!;
 
 		// We only want to set dependent devices offline when the gateway goes
 		// offline, when the gateway comes back it's its job to set the dependent
 		// device back to online as need be.
-		const isOnline = args.request.values.is_online;
+		const isOnline = request.values.is_online;
 		if ([false, 0].includes(isOnline) && affectedIds.length !== 0) {
-			await args.api.patch({
+			await api.patch({
 				resource: 'device',
 				options: {
 					$filter: {
