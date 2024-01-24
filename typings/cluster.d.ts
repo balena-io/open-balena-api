190a0ba544c781892e60fbd9f455f9e6fee9e253
Pagan Gazzard <page@balena.io>
2024-01-24 14:06:15 +0000

    Remove now unnecessary cluster typings
    Change-type: patch

--- typings/cluster.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'cluster' {
-	// This is required to allow us to import cluster correctly since the default typings now expect use of either
-	// es modules or `esModuleInterop: true`
-	const cluster: Cluster;
-	export = cluster;
-}
