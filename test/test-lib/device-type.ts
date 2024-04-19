7978ea2273e583627bed31870ac2a0490217d660
Pagan Gazzard <page@balena.io>
2024-04-19 12:32:53 +0100

    Fix typing of the device-type proxy type
    It returns an object of promises and we now correctly type that

--- test/test-lib/device-type.ts
+++ test/test-lib/device-type.ts
@@ -7,32 +7,35 @@ const { api } = sbvrUtils;
 export const loadDefaultFixtures = () =>
 	setDefaultFixtures(
 		'deviceTypes',
-		new Proxy({} as Dictionary<Pick<DeviceType, 'id' | 'slug' | 'name'>>, {
-			get: async (obj, slug) => {
-				if (
-					typeof slug === 'string' &&
-					!Object.prototype.hasOwnProperty.call(obj, slug)
-				) {
-					if (slug === 'then') {
-						// Something is checking if we're a thenable
-						return;
+		new Proxy(
+			{} as Dictionary<Promise<Pick<DeviceType, 'id' | 'slug' | 'name'>>>,
+			{
+				get: (obj, slug) => {
+					if (
+						typeof slug === 'string' &&
+						!Object.prototype.hasOwnProperty.call(obj, slug)
+					) {
+						if (slug === 'then') {
+							// Something is checking if we're a thenable
+							return;
+						}
+						const deviceType = api.resin.get({
+							resource: 'device_type',
+							passthrough: {
+								req: permissions.root,
+							},
+							id: {
+								slug,
+							},
+							options: {
+								$select: ['id', 'slug', 'name'],
+							},
+						}) as Promise<Pick<DeviceType, 'id' | 'slug' | 'name'>>;
+						obj[slug] = deviceType;
 					}
-					const deviceType = (await api.resin.get({
-						resource: 'device_type',
-						passthrough: {
-							req: permissions.root,
-						},
-						id: {
-							slug,
-						},
-						options: {
-							$select: ['id', 'slug', 'name'],
-						},
-					})) as Pick<DeviceType, 'id' | 'slug' | 'name'>;
-					obj[slug] = deviceType;
-				}
 
-				return obj[slug as any];
+					return obj[slug as keyof typeof obj];
+				},
 			},
-		}) as any as Dictionary<Promise<Pick<DeviceType, 'id' | 'slug' | 'name'>>>,
+		),
 	);
