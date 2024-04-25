f2c400123e96b9e763b61bd8fdc9483076c7a566
Anton Belodedenko <2033996+ab77@users.noreply.github.com>
2024-04-25 09:38:58 -0700

    correct startup when run as a standalone container
    change-type: patch

--- entry.sh
+++ entry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$PRODUCTION_MODE" == "true" ]; then
-	exec node --loader ts-node/esm/transpile-only index.js
+	exec node --enable-source-maps --loader ts-node/esm/transpile-only index.js
 else
-	exec ./node_modules/.bin/supervisor \
+	exec node_modules/.bin/supervisor \
 		--no-restart-on error \
 		--extensions js,node,coffee,sbvr,json,sql,pegjs,ts \
 		--watch src \
-		--exec node --loader ts-node/esm/transpile-only index.js
+		-- --enable-source-maps --require ts-node/register --loader ts-node/esm/transpile-only index.js
 fi
