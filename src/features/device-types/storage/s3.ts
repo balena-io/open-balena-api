fc5f0709dd79854d72c42fecf89271f35c05e0bb
Josh Bowling <josh@balena.io>
2024-05-28 18:18:02 +0900

    Update s3 error handling and logs
    Change-type: patch

--- src/features/device-types/storage/s3.ts
+++ src/features/device-types/storage/s3.ts
@@ -57,19 +57,20 @@ function createS3Client() {
 
 const s3Client = createS3Client();
 
-function logUnauthenticatedWarning(
+function isUnauthenticatedError(
 	clientS3: UnauthenticatedS3Facade | AWSWrapper.AWS.S3,
-	pathS3: string,
 	err: any,
-): void {
-	if (
+): boolean {
+	return (
 		clientS3 instanceof UnauthenticatedS3Facade &&
 		[401, 403].includes(err.statusCode)
-	) {
-		console.warn(
-			`${err.code} (${err.statusCode}): ${pathS3} belongs to a private device type or has incorrect permissions`,
-		);
-	}
+	);
+}
+
+function logUnauthenticated(pathS3: string, err: any): void {
+	console.warn(
+		`${err.code} (${err.statusCode}): ${pathS3} belongs to a private device type or has incorrect permissions`,
+	);
 }
 
 async function getFileInfo(s3Path: string) {
@@ -88,8 +89,12 @@ export async function getFile(s3Path: string) {
 		});
 		return await req.promise();
 	} catch (err) {
-		// catch errors for private device types when running unauthenticated
-		logUnauthenticatedWarning(s3Client, s3Path, err);
+		if (isUnauthenticatedError(s3Client, err)) {
+			// catch errors for private device types when running unauthenticated
+			logUnauthenticated(s3Path, err);
+		} else {
+			throw err;
+		}
 	}
 }
 
@@ -148,9 +153,10 @@ export async function fileExists(s3Path: string): Promise<boolean> {
 		await getFileInfo(s3Path);
 		return true;
 	} catch (err) {
-		// catch errors for private device types when running unauthenticated
-		logUnauthenticatedWarning(s3Client, s3Path, err);
-		if (err.statusCode === 404) {
+		if (isUnauthenticatedError(s3Client, err)) {
+			// catch errors for private device types when running unauthenticated
+			logUnauthenticated(s3Path, err);
+		} else if (err.statusCode === 404) {
 			return false;
 		}
 		throw err;
