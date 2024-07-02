8a71fa2ff9f001da7fb84419fc42909677954ee0
Andrea Rosci <jsreds@Andreas-MBP.lan>
2024-07-02 15:59:01 +0200

    is_pinned_on__release: stop setting device.should_be_running__release and remove it from the sbvr
    Change-type: patch

--- src/features/devices/hooks/defaults-validation.ts
+++ src/features/devices/hooks/defaults-validation.ts
@@ -29,16 +29,6 @@ hooks.addPureHook('POST', 'resin', 'device', {
 				'Device UUID must be a 32 or 62 character long lower case hex string.',
 			);
 		}
-
-		// TODO[device management next step]: Drop this after re-migrating all data on step 2:
-		if (request.values.is_pinned_on__release !== undefined) {
-			// Add an async boundary so that value updates,
-			// and doesn't remove the properties that we add.
-			// eslint-disable-next-line @typescript-eslint/await-thenable
-			await null;
-			request.values.should_be_running__release =
-				request.values.is_pinned_on__release;
-		}
 	},
 });
 
@@ -82,16 +72,6 @@ hooks.addPureHook('PATCH', 'resin', 'device', {
 
 		if (request.values.is_online != null) {
 			request.values.last_connectivity_event = new Date();
-		}
-
-		// TODO[device management next step]: Drop this after re-migrating all data on step 2:
-		if (request.values.is_pinned_on__release !== undefined) {
-			// Add an async boundary so that value updates,
-			// and doesn't remove the properties that we add.
-			// eslint-disable-next-line @typescript-eslint/await-thenable
-			await null;
-			request.values.should_be_running__release =
-				request.values.is_pinned_on__release;
 		}
 	},
 });
