f549051d4772be62e81abf38dace7265b51153e4
Josh Bowling <josh@balena.io>
2024-08-09 08:40:51 +0900

    Update @balena/pinejs to v18
    Change-type: patch

--- src/features/applications/index.ts
+++ src/features/applications/index.ts
@@ -1,13 +1,15 @@
+import { errors } from '@balena/pinejs';
+
 export const getApplicationSlug = (
 	orgHandle: string,
 	appName: string,
 ): string => {
 	if (orgHandle == null) {
-		throw new Error('Missing organization handle');
+		throw new errors.BadRequestError('Missing organization handle');
 	}
 
 	if (appName == null) {
-		throw new Error('Missing application name');
+		throw new errors.BadRequestError('Missing application name');
 	}
 
 	return `${orgHandle}/${appName}`.toLowerCase();
