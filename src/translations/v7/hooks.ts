a20e9ab16f25a3b1f0329f7a5dc85935a5c1b5c2
Otavio Jacobi <otaviojacobi@gmail.com>
2024-12-10 08:24:29 -0300

    Stop filling service install on device service environment variable
    Change-type: patch

--- src/translations/v7/hooks.ts
+++ src/translations/v7/hooks.ts
@@ -1,6 +1,52 @@
 import { addDeleteHookForDependents } from '../../infra/cascade-delete/index.js';
+import { hooks, sbvrUtils, errors } from '@balena/pinejs';
+
+const addReadOnlyHook = (
+	methods: Array<Parameters<typeof hooks.addHook>[0]>,
+	resource: string,
+	hook: sbvrUtils.Hooks<'v7'>,
+) => {
+	methods.map((method) => {
+		hooks.addHook(method, 'v7', resource, {
+			...hook,
+			sideEffects: false,
+			readOnlyTx: true,
+		});
+	});
+};
 
 // Service install resource should only be used for <= v7 translations
 addDeleteHookForDependents('v7', 'service_install', {
 	device_service_environment_variable: 'service_install',
 });
+
+addReadOnlyHook(
+	['POST', 'PATCH', 'PUT'],
+	'device_service_environment_variable',
+	{
+		POSTPARSE: async ({ request, api }) => {
+			const { service_install: siId } = request.values;
+
+			if (siId == null) {
+				return;
+			}
+
+			const si = await sbvrUtils.api.resin.get({
+				resource: 'service_install',
+				passthrough: api.passthrough,
+				id: siId,
+				options: {
+					$select: ['device', 'service'],
+				},
+			});
+
+			if (si == null) {
+				throw new errors.UnauthorizedError();
+			}
+
+			request.values.device = si.device.__id;
+			request.values.service = si.service.__id;
+			delete request.values.service_install;
+		},
+	},
+);
