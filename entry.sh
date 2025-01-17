fc873ef26ef6d90dc45a20ce466112cdd2c3e8b6
Pagan Gazzard <page@balena.io>
2025-01-17 15:30:59 +0000

    Switch from ts-node to @swc-node/register
    Change-type: patch

--- entry.sh
+++ entry.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$PRODUCTION_MODE" == "true" ]; then
-	exec node --enable-source-maps --loader ts-node/esm/transpile-only index.js
+	exec node --enable-source-maps --loader @swc-node/register/esm-register index.js
 else
 	exec node_modules/.bin/supervisor \
 		--no-restart-on error \
 		--extensions js,node,coffee,sbvr,json,sql,pegjs,ts \
 		--watch src \
-		-- --enable-source-maps --require ts-node/register --loader ts-node/esm/transpile-only index.js
+		-- --enable-source-maps --loader @swc-node/register/esm-register index.js
 fi
