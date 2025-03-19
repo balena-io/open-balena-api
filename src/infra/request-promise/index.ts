c5efcab8d22f3cf9d7b833b08e51eef3fa494383
Pagan Gazzard <page@balena.io>
2025-03-19 14:14:56 +0000

    Switch to native promises for the request wrapper
    Change-type: major

--- src/infra/request-promise/index.ts
+++ src/infra/request-promise/index.ts
@@ -1,54 +1,27 @@
-import Bluebird from 'bluebird';
 import request from 'request';
 
 import { EXTERNAL_HTTP_TIMEOUT_MS } from '../../lib/config.js';
 
-type Request = typeof request;
-
 export type RequestResponse = [request.Response, any];
 
-interface PromisifiedRequest extends Request {
-	getAsync(
-		options:
-			| (request.UriOptions & request.CoreOptions)
-			| (request.UrlOptions & request.CoreOptions),
-	): Bluebird<RequestResponse>;
-	getAsync(
-		uri: string,
-		options?: request.CoreOptions,
-	): Bluebird<RequestResponse>;
-
-	postAsync: (
-		arg1:
-			| (request.UriOptions & request.CoreOptions)
-			| (request.UrlOptions & request.CoreOptions),
-	) => Bluebird<RequestResponse>;
-	putAsync: (
-		arg1:
-			| (request.UriOptions & request.CoreOptions)
-			| (request.UrlOptions & request.CoreOptions),
-	) => Bluebird<RequestResponse>;
-	delAsync: (
-		arg1:
-			| (request.UriOptions & request.CoreOptions)
-			| (request.UrlOptions & request.CoreOptions),
-	) => Bluebird<RequestResponse>;
-}
-
-export const defaultRequest = request.defaults({
+const defaultRequest = request.defaults({
 	timeout: EXTERNAL_HTTP_TIMEOUT_MS,
 });
 
-const promisifiedRequest = Bluebird.promisifyAll(defaultRequest, {
-	multiArgs: true,
-}) as any as PromisifiedRequest;
-
-export const requestAsync = Bluebird.promisify(promisifiedRequest, {
-	multiArgs: true,
-}) as any as (
+export const requestAsync = (
 	arg1:
 		| (request.UriOptions & request.CoreOptions)
 		| (request.UrlOptions & request.CoreOptions),
-) => Bluebird<RequestResponse>;
+) => {
+	return new Promise<RequestResponse>((resolve, reject) => {
+		defaultRequest(arg1, (err: Error, res, body) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve([res, body]);
+		});
+	});
+};
 
-export default promisifiedRequest;
+export default defaultRequest;
