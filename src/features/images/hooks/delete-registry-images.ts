21f0f488eb4d428b608a823fa82b943c0ff9ac19
joshbwlng <josh@balena.io>
2025-09-30 08:45:57 +0900

    Mark images for deletion on release deletion
    Change-type: minor

---
+++ src/features/images/hooks/delete-registry-images.ts
@@ -0,0 +1,77 @@
+import { sbvrUtils, hooks, permissions } from '@balena/pinejs';
+import _ from 'lodash';
+import type { DeleteRegistryImagesTaskParams } from '../tasks/delete-registry-images.js';
+import {
+	ASYNC_TASK_ATTEMPT_LIMIT,
+	ASYNC_TASK_DELETE_REGISTRY_IMAGES_BATCH_SIZE,
+	ASYNC_TASK_DELETE_REGISTRY_IMAGES_ENABLED,
+	ASYNC_TASK_DELETE_REGISTRY_IMAGES_OFFSET_SECONDS,
+	ASYNC_TASKS_ENABLED,
+} from '../../../lib/config.js';
+
+interface DeleteRequestCustomObject {
+	imagesToCleanup?: DeleteRegistryImagesTaskParams['images'];
+}
+
+if (ASYNC_TASKS_ENABLED && ASYNC_TASK_DELETE_REGISTRY_IMAGES_ENABLED) {
+	hooks.addPureHook('DELETE', 'resin', 'image', {
+		PRERUN: async (args) => {
+			const { api, request } = args;
+			const affectedIds = await sbvrUtils.getAffectedIds(args);
+			if (affectedIds.length === 0) {
+				return;
+			}
+
+			// Get information required to mark the image for deletion on
+			// the registry before the record is deleted from our database.
+			const images = await api.get({
+				resource: 'image',
+				options: {
+					$select: ['is_stored_at__image_location', 'content_hash'],
+					$filter: {
+						id: { $in: affectedIds },
+						content_hash: { $ne: null },
+						status: 'success',
+					},
+				},
+			});
+			if (images.length > 0) {
+				(request.custom as DeleteRequestCustomObject).imagesToCleanup =
+					images.map((image) => [
+						image.is_stored_at__image_location,
+						image.content_hash!,
+					]);
+			}
+		},
+		POSTRUN: async ({ request, tx }) => {
+			const { imagesToCleanup } = request.custom as DeleteRequestCustomObject;
+			if (imagesToCleanup == null || imagesToCleanup.length === 0) {
+				return;
+			}
+
+			const chunks = _.chunk(
+				imagesToCleanup,
+				ASYNC_TASK_DELETE_REGISTRY_IMAGES_BATCH_SIZE,
+			);
+			await Promise.all(
+				chunks.map((chunk) =>
+					sbvrUtils.api.tasks.post({
+						resource: 'task',
+						passthrough: { req: permissions.root, tx },
+						body: {
+							is_executed_by__handler: 'delete_registry_images',
+							is_executed_with__parameter_set: {
+								images: chunk,
+							} satisfies DeleteRegistryImagesTaskParams,
+							is_scheduled_to_execute_on__time: new Date(
+								Date.now() +
+									ASYNC_TASK_DELETE_REGISTRY_IMAGES_OFFSET_SECONDS * 1000,
+							),
+							attempt_limit: ASYNC_TASK_ATTEMPT_LIMIT,
+						},
+					}),
+				),
+			);
+		},
+	});
+}
