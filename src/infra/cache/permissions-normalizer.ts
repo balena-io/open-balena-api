e40bdbe2a5d03982175a11653e424edda33e4ff6
Pagan Gazzard <page@balena.io>
2025-11-13 21:06:25 +0000

    Pre-allocate a buffer to improve performance for `reqPermissionNormalizer`
    This avoids constantly creating new buffers for each call and the
    associated GC work
    
    Change-type: patch

--- src/infra/cache/permissions-normalizer.ts
+++ src/infra/cache/permissions-normalizer.ts
@@ -1,11 +1,13 @@
 import type { permissions } from '@balena/pinejs';
 import fnv1a from '@sindresorhus/fnv1a';
 
+// Pre-allocate a buffer to improve performance when hashing permissions / reduce allocations
+const opts = { utf8Buffer: new Uint8Array(256) };
 const hashPermissions = (permissions: string[] | undefined): string => {
 	if (permissions == null) {
 		return '';
 	}
-	return `${fnv1a(permissions.join()).toString(36)}${permissions.length}`;
+	return `${fnv1a(permissions.join(), opts).toString(36)}${permissions.length}`;
 };
 
 export const reqPermissionNormalizer = (req: permissions.PermissionReq) => {
