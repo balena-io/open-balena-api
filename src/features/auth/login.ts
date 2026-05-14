65a62582bdb17af5100ae22fe0a8d06e9294f4de
Pagan Gazzard <page@balena.io>
2026-05-14 21:45:19 +0100

    Add `zod` based validation for `/login_` endpoint
    https://balena.fibery.io/Work/Project/1002
    
    Change-type: patch

--- src/features/auth/login.ts
+++ src/features/auth/login.ts
@@ -1,4 +1,3 @@
-import type { RequestHandler } from 'express';
 import { errors, sbvrUtils } from '@balena/pinejs';
 import { comparePassword, findUser } from '../../infra/auth/auth.js';
 import { loginUserXHR } from '../../infra/auth/jwt.js';
@@ -6,47 +5,58 @@ import { captureException } from '../../infra/error-handling/index.js';
 import { permissions } from '@balena/pinejs';
 
 import type { SetupOptions } from '../../index.js';
+import {
+	createValidatedRequestHandler,
+	z,
+} from '../../infra/validation/index.js';
 
 const { BadRequestError, NotFoundError } = errors;
 
-export const login =
-	(onLogin: SetupOptions['onLogin']): RequestHandler =>
-	async (req, res) => {
-		const { username, password } = req.body;
+export const login = (onLogin: SetupOptions['onLogin']) =>
+	createValidatedRequestHandler(
+		{
+			body: z.object({
+				username: z.string(),
+				password: z.string(),
+			}),
+		},
+		async (req, res) => {
+			const { username, password } = req.body;
 
-		if (!(username && password)) {
-			return res.status(401).end();
-		}
-
-		try {
-			await sbvrUtils.db.readTransaction(async (tx) => {
-				const user = await findUser(username, tx);
-				if (!user) {
-					throw new NotFoundError('User not found.');
-				}
-
-				const matches = await comparePassword(password, user.password);
-				if (!matches) {
-					throw new BadRequestError('Current password incorrect.');
-				}
-
-				const userPermissions = await permissions.getUserPermissions(user.id);
-				if (!userPermissions.includes('auth.credentials_login')) {
-					throw new BadRequestError('User not allowed to login.');
-				}
-
-				if (onLogin) {
-					await onLogin(user, tx, req);
-				}
-				await req.resetRatelimit?.();
-				await loginUserXHR(res, user.id, { tx });
-			});
-		} catch (err) {
-			if (err instanceof BadRequestError || err instanceof NotFoundError) {
-				res.status(401).end();
-				return;
+			if (!(username && password)) {
+				return res.status(401).end();
 			}
-			captureException(err, 'Error logging in');
-			res.status(401).end();
-		}
-	};
+
+			try {
+				await sbvrUtils.db.readTransaction(async (tx) => {
+					const user = await findUser(username, tx);
+					if (!user) {
+						throw new NotFoundError('User not found.');
+					}
+
+					const matches = await comparePassword(password, user.password);
+					if (!matches) {
+						throw new BadRequestError('Current password incorrect.');
+					}
+
+					const userPermissions = await permissions.getUserPermissions(user.id);
+					if (!userPermissions.includes('auth.credentials_login')) {
+						throw new BadRequestError('User not allowed to login.');
+					}
+
+					if (onLogin) {
+						await onLogin(user, tx, req);
+					}
+					await req.resetRatelimit?.();
+					await loginUserXHR(res, user.id, { tx });
+				});
+			} catch (err) {
+				if (err instanceof BadRequestError || err instanceof NotFoundError) {
+					res.status(401).end();
+					return;
+				}
+				captureException(err, 'Error logging in');
+				res.status(401).end();
+			}
+		},
+	);
