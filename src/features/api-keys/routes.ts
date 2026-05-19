4daed307f2bb81747d80b59df6ea84759a67f21f
Pagan Gazzard <page@balena.io>
2026-05-19 17:24:16 +0100

    Add `zod` based validation for api key creation endpoints
    https://balena.fibery.io/Work/Project/1002
    
    Change-type: patch

--- src/features/api-keys/routes.ts
+++ src/features/api-keys/routes.ts
@@ -1,6 +1,4 @@
 import { errors, sbvrUtils } from '@balena/pinejs';
-import type { RequestHandler } from 'express';
-
 import { getUser } from '../../infra/auth/auth.js';
 import {
 	captureException,
@@ -17,153 +15,183 @@ import {
 	getApiKeyOptsFromRequest,
 	supportedActorTypes,
 } from './lib.js';
+import {
+	createValidatedRequestHandler,
+	z,
+} from '../../infra/validation/index.js';
 
-export const createGenericApiKey = (version: 'v1' | 'v2'): RequestHandler => {
+export const createGenericApiKey = (version: 'v1' | 'v2') => {
 	const mandatoryExpiryDate = version !== 'v1';
-	return async (req, res) => {
-		try {
-			const apiKeyOptions = getApiKeyOptsFromRequest(
-				req.body,
-				undefined,
-				mandatoryExpiryDate,
-			);
-			const {
-				actorType,
-				actorTypeId,
-				roles,
-				apiKey: chosenApiKey,
-			} = req.body as Dictionary<unknown>;
-
-			if (
-				typeof actorType !== 'string' ||
-				!supportedActorTypes.includes(
-					actorType as (typeof supportedActorTypes)[number],
-				)
-			) {
-				throw new errors.BadRequestError('Unsupported actor type');
-			}
-			if (typeof actorTypeId !== 'number' || !Number.isFinite(actorTypeId)) {
-				throw new errors.BadRequestError('Actor type id must be a number');
-			}
-			if (
-				!Array.isArray(roles) ||
-				roles.length === 0 ||
-				roles.some((r) => typeof r !== 'string' || r.length === 0)
-			) {
-				throw new errors.BadRequestError(
-					'Roles should be an array of role names',
+	return createValidatedRequestHandler(
+		{
+			body: z.object({
+				actorType: z.enum(supportedActorTypes),
+				actorTypeId: z.number(),
+				roles: z.array(z.string().min(1)).min(1),
+				apiKey: z.string().nullish(),
+				// These are for `getApiKeyOptsFromRequest`
+				name: z.string().nullish(),
+				description: z.string().nullish(),
+				expiryDate: z.string().nullish(),
+			}),
+		},
+		async (req, res) => {
+			try {
+				const apiKeyOptions = getApiKeyOptsFromRequest(
+					req.body,
+					undefined,
+					mandatoryExpiryDate,
 				);
-			}
+				const {
+					actorType,
+					actorTypeId,
+					roles,
+					apiKey: chosenApiKey,
+				} = req.body;
 
-			if (chosenApiKey != null && typeof chosenApiKey !== 'string') {
-				throw new errors.BadRequestError('API key must be a string');
+				const apiKey = await $createGenericApiKey(req, {
+					actorType,
+					actorTypeId,
+					roles,
+					apiKey: chosenApiKey ?? undefined,
+					...apiKeyOptions,
+				});
+				res.json(apiKey);
+			} catch (err) {
+				if (handleHttpErrors(req, res, err)) {
+					return;
+				}
+				captureException(err, 'Error generating API key');
+				res.status(500).send(translateError(err));
 			}
+		},
+	);
+};
 
-			const apiKey = await $createGenericApiKey(req, {
-				actorType: actorType as (typeof supportedActorTypes)[number],
-				actorTypeId,
-				roles,
-				apiKey: chosenApiKey ?? undefined,
-				...apiKeyOptions,
+export const createDeviceApiKey = createValidatedRequestHandler(
+	{
+		body: z.object({
+			apiKey: z.string().nullish(),
+			// These are for `getApiKeyOptsFromRequest`
+			name: z.string().nullish(),
+			description: z.string().nullish(),
+			expiryDate: z.string().nullish(),
+		}),
+	},
+	async (req, res) => {
+		const deviceId = parseInt(req.params.deviceId, 10);
+		if (!Number.isFinite(deviceId)) {
+			res.status(400).send('Device id must be a number');
+			return;
+		}
+
+		try {
+			const apiKey = await $createDeviceApiKey(req, deviceId, {
+				apiKey: req.body.apiKey ?? undefined,
+				...getApiKeyOptsFromRequest(req.body),
 			});
 			res.json(apiKey);
 		} catch (err) {
 			if (handleHttpErrors(req, res, err)) {
 				return;
 			}
-			captureException(err, 'Error generating API key');
+			captureException(err, 'Error generating device API key');
 			res.status(500).send(translateError(err));
 		}
-	};
-};
+	},
+);
 
-export const createDeviceApiKey: RequestHandler = async (req, res) => {
-	const deviceId = parseInt(req.params.deviceId, 10);
-	if (!Number.isFinite(deviceId)) {
-		res.status(400).send('Device id must be a number');
-		return;
-	}
-
-	try {
-		const apiKey = await $createDeviceApiKey(req, deviceId, {
-			apiKey: req.body.apiKey,
-			...getApiKeyOptsFromRequest(req.body),
-		});
-		res.json(apiKey);
-	} catch (err) {
-		if (handleHttpErrors(req, res, err)) {
+export const createProvisioningApiKey = createValidatedRequestHandler(
+	{
+		body: z.object({
+			// These are for `getApiKeyOptsFromRequest`
+			name: z.string().nullish(),
+			description: z.string().nullish(),
+			expiryDate: z.string().nullish(),
+		}),
+	},
+	async (req, res) => {
+		const appId = parseInt(req.params.appId, 10);
+		if (!Number.isFinite(appId)) {
+			res.status(400).send('Application id must be a number');
 			return;
 		}
-		captureException(err, 'Error generating device API key');
-		res.status(500).send(translateError(err));
-	}
-};
 
-export const createProvisioningApiKey: RequestHandler = async (req, res) => {
-	const appId = parseInt(req.params.appId, 10);
-	if (!Number.isFinite(appId)) {
-		res.status(400).send('Application id must be a number');
-		return;
-	}
-
-	try {
-		const apiKey = await $createProvisioningApiKey(
-			req,
-			appId,
-			getApiKeyOptsFromRequest(req.body),
-		);
-		res.json(apiKey);
-	} catch (err) {
-		if (handleHttpErrors(req, res, err)) {
-			return;
+		try {
+			const apiKey = await $createProvisioningApiKey(
+				req,
+				appId,
+				getApiKeyOptsFromRequest(req.body),
+			);
+			res.json(apiKey);
+		} catch (err) {
+			if (handleHttpErrors(req, res, err)) {
+				return;
+			}
+			captureException(err, 'Error generating provisioning API key');
+			res.status(500).send(translateError(err));
 		}
-		captureException(err, 'Error generating provisioning API key');
-		res.status(500).send(translateError(err));
-	}
-};
+	},
+);
 
 /**
  * @deprecated this is a legacy api key for very old devices and should not be used any more
  */
-export const createUserApiKey: RequestHandler = async (req, res) => {
-	try {
-		const keyMetadata = getApiKeyOptsFromRequest(req.body);
+export const createUserApiKey = createValidatedRequestHandler(
+	{
+		body: z.object({
+			// These are for `getApiKeyOptsFromRequest`
+			name: z.string().nullish(),
+			description: z.string().nullish(),
+			expiryDate: z.string().nullish(),
+		}),
+	},
+	async (req, res) => {
+		try {
+			const keyMetadata = getApiKeyOptsFromRequest(req.body);
 
-		const apiKey = await sbvrUtils.db.transaction(async (tx) => {
-			const user = await getUser(req, tx);
-			return await $createUserApiKey(req, user.id, { tx, ...keyMetadata });
-		});
-		res.json(apiKey);
-	} catch (err) {
-		if (handleHttpErrors(req, res, err)) {
-			return;
+			const apiKey = await sbvrUtils.db.transaction(async (tx) => {
+				const user = await getUser(req, tx);
+				return await $createUserApiKey(req, user.id, { tx, ...keyMetadata });
+			});
+			res.json(apiKey);
+		} catch (err) {
+			if (handleHttpErrors(req, res, err)) {
+				return;
+			}
+			captureException(err, 'Error generating user API key');
+			res.status(500).send(translateError(err));
 		}
-		captureException(err, 'Error generating user API key');
-		res.status(500).send(translateError(err));
-	}
-};
+	},
+);
 
-export const createNamedUserApiKey: RequestHandler = async (req, res) => {
-	try {
-		if (!req.body.name) {
-			throw new errors.BadRequestError('API keys require a name');
-		}
+export const createNamedUserApiKey = createValidatedRequestHandler(
+	{
+		body: z.object({
+			name: z.string().min(1),
+			// These are for `getApiKeyOptsFromRequest`
+			description: z.string().nullish(),
+			expiryDate: z.string().nullish(),
+		}),
+	},
+	async (req, res) => {
+		try {
+			if (req.user == null) {
+				throw new errors.BadRequestError('Must use user auth');
+			}
 
-		if (req.user == null) {
-			throw new errors.BadRequestError('Must use user auth');
+			const apiKey = await $createNamedUserApiKey(
+				req,
+				req.user.id,
+				getApiKeyOptsFromRequest(req.body),
+			);
+			res.json(apiKey);
+		} catch (err) {
+			if (handleHttpErrors(req, res, err)) {
+				return;
+			}
+			captureException(err, 'Error generating named user API key');
+			res.status(500).send(translateError(err));
 		}
-
-		const apiKey = await $createNamedUserApiKey(
-			req,
-			req.user.id,
-			getApiKeyOptsFromRequest(req.body),
-		);
-		res.json(apiKey);
-	} catch (err) {
-		if (handleHttpErrors(req, res, err)) {
-			return;
-		}
-		captureException(err, 'Error generating named user API key');
-		res.status(500).send(translateError(err));
-	}
-};
+	},
+);
